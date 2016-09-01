046042c5fb10166f877d17bcd98d8ba8c4b107d1
nisha <nisha.3ginfo@gmail.com>
2016-09-01 16:03:36 +0530

    send file

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+//adding
+	define('SHOPIFY_APP_API_KEY', 'ecd4248755a8d2d90c9ae1de56646af0');
+	define('SHOPIFY_APP_SHARED_SECRET', 'f4c8a0e9897ca4faa3370549c1d34747');
+// Create the API URL using the private app credentials
+$api_url = 'https://4024250ac11658138b132db0590e0cfe:bf15a7ba9c02040bf26fd59358c42dde@apptest-32.myshopify.com';
