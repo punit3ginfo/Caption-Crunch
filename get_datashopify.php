0f42a4784215cbb7f2d969d29d1bd32cd4a2a0d9
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-10 15:03:30 +0530

    Create get_datashopify.php

---
+++ get_datashopify.php
@@ -0,0 +1,11 @@
+<?php
+$webhookContent = "";
+
+$webhook = fopen('php://input' , 'rb');
+while (!feof($webhook)) {
+    $webhookContent .= fread($webhook, 4096);
+}
+fclose($webhook);
+
+error_log($webhookContent);
+?>
