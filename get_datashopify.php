fbe147130ec818b52600e1c2dfba5e60206fbc68
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-12 16:10:07 +0530

    Update get_datashopify.php

--- get_datashopify.php
+++ get_datashopify.php
@@ -13,7 +13,7 @@ $data = file_get_contents('php://input');
 $verified = verify_webhook($data, $hmac_header);
 error_log('Webhook verified: '.var_export($verified, true)); */
 //check error.log to see the result
-echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('3','1003','nisha','test','teste','40')");
+//echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('3','1003','nisha','test','teste','40')");
 $webhookContent = "";
 $webhook = fopen('php://input' , 'rb');
 while (!feof($webhook)) {
@@ -23,17 +23,19 @@ $data = json_decode($webhook, true);
 $p1 = $data['order']; 
 foreach($p1 as $val)
 {
-	$order_id = $data['order']['id'];
-    $first_name = $data['order']['name'];
-    $last_name = $data['order']['name'];
-    $address = $data['order']['address']['streetaddress'];
-    $order_amount = $data['order']['total_price'];
+	$order_id = $data['id'];
+    $first_name = $data['name'];
+    $last_name = $data['name'];
+    $address = $data['address']['streetaddress'];
+    $order_amount = $data['total_price'];
 	if (!$dbconn4) {
 	 exit;
 	}
-    pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('1',".$order_id.",".$first_name.",".$last_name.",".$address.",".$order_amount.")");
+	echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4','1004','nisha4','test','teste','40')");
+    pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4',".$order_id.",".$first_name.",".$last_name.",".$address.",".$order_amount.")");
    
 }
+echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('5','1005','nisha5','test','teste','40')");
 fclose($webhook);
 error_log($webhookContent);
 ?>
