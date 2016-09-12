649eb4ffdf8c4864ac91b15b2aa7a0b1efd0d7a7
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-12 11:13:39 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php 
+$conn_string = "host=ec2-174-129-223-35.compute-1.amazonaws.com port=5432 dbname=dcdhmp2hbn5ol8 user=iorgedgwzuhxvh password=HtexMOv8jCwSmlLI678h34VFNT";
+$dbconn4 = pg_connect($conn_string);
+//connect to a database named "dcdhmp2hbn5ol8" on the host "ec2-174-129-223-35.compute-1.amazonaws.com" with a username and password
+
+?>
