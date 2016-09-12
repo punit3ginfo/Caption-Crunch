32db66d2d26990e39746bb7720ad424ed231b1fd
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-12 17:29:29 +0530

    Update get_datashopify.php

--- get_datashopify.php
+++ get_datashopify.php
@@ -16,6 +16,9 @@ error_log('Webhook verified: '.var_export($verified, true)); */
 //echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('3','1003','nisha','test','teste','40')");
 $webhookContent = "";
 $webhook = fopen('php://input' , 'rb');
+$file = fopen("test.txt","w");
+echo fwrite($file,$webhook);
+fclose($file);
 while (!feof($webhook)) {
     $webhookContent .= fread($webhook, 4096);
 }
@@ -23,19 +26,19 @@ $data = json_decode($webhook, true);
 $p1 = $data['order']; 
 foreach($p1 as $val)
 {
-	$order_id = $data['id'];
-    $first_name = $data['name'];
-    $last_name = $data['name'];
-    $address = $data['address']['streetaddress'];
-    $order_amount = $data['total_price'];
+	$order_id = $val['id'];
+    $first_name = $val['name'];
+    $last_name = $val['name'];
+    $address = $val['address']['streetaddress'];
+    $order_amount = $val['total_price'];
 	if (!$dbconn4) {
 	 exit;
 	}
-	echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4','1004','nisha4','test','teste','40')");
+	echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4','1004','nisha4','test','teste','$40')");
     pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4',".$order_id.",".$first_name.",".$last_name.",".$address.",".$order_amount.")");
    
 }
-echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('5','1005','nisha5','test','teste','40')");
+//echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('5','1005','nisha5','test','teste','40')");
 fclose($webhook);
 error_log($webhookContent);
 ?>
