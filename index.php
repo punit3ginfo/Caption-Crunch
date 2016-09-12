59f18d4e546da2430e44d997e5c126dffdc921ed
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-12 12:21:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
 require __DIR__.'/conf.php'; 
 require __DIR__.'/connection.php'; 
+$conn_string = "host=ec2-174-129-223-35.compute-1.amazonaws.com port=5432 dbname=dcdhmp2hbn5ol8 user=iorgedgwzuhxvh password=HtexMOv8jCwSmlLI678h34VFNT";
+$dbconn4 = pg_connect($conn_string);
+//connect to a database named "dcdhmp2hbn5ol8" on the host "ec2-174-129-223-35.compute-1.amazonaws.com" with a username and password
+if(!$dbconn4){ 
+	echo "Error : Unable to open database\n"; 
+}
 	?>
