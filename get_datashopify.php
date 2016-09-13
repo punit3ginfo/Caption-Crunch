6aca809a75abe45d3306c70f4686ea8e4c9b0f23
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 15:39:22 +0530

    Update get_datashopify.php

--- get_datashopify.php
+++ get_datashopify.php
@@ -1,44 +1,25 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-	use phpish\shopify;
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require __DIR__.'/conf.php'; 
-require __DIR__.'/connection.php'; 
-/*function verify_webhook($data, $hmac_header)
-{
-	$calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
-	return ($hmac_header == $calculated_hmac);
-}
-$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
-$data = file_get_contents('php://input');
-$verified = verify_webhook($data, $hmac_header);
-error_log('Webhook verified: '.var_export($verified, true)); */
-//check error.log to see the result
-//echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('3','1003','nisha','test','teste','40')");
-$webhookContent = "";
-$webhook = fopen('php://input' , 'rb');
-$file = fopen("test.txt","w");
-echo fwrite($file,$webhook);
-fclose($file);
-while (!feof($webhook)) {
-    $webhookContent .= fread($webhook, 4096);
-}
-$data = json_decode($webhook, true);
-$p1 = $data['order']; 
-foreach($p1 as $val)
-{
-	$order_id = $val['id'];
-    $first_name = $val['name'];
-    $last_name = $val['name'];
-    $address = $val['address']['streetaddress'];
-    $order_amount = $val['total_price'];
-	if (!$dbconn4) {
-	 exit;
+
+$actionTemp = explode('::', $_REQUEST['action']);
+$action = $actionTemp[0];
+$store = $actionTemp[1];
+$access_token = shopify\access_token($store, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, 'ff765ceb4cc0deaf246f3895d906c1c8');
+$access_token = $access_token;
+$storeData = json_decode(file_get_contents("https://{$store}/admin/shop.json?access_token={$access_token}"));
+if(!empty($action)){
+	$data = '';
+	$webhook = fopen('php://input' , 'rb'); 
+	while(!feof($webhook)){
+		$data .= fread($webhook, 4096); 
 	}
-	echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4','1004','nisha4','test','teste','$40')");
-    pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('4',".$order_id.",".$first_name.",".$last_name.",".$address.",".$order_amount.")");
-   
+	fclose($webhook);
+	$data = json_decode($data);
+	
+	 pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,first_name,last_name,address) VALUES ('4',".$data->first_name.",".$data->last_name.",".$access_token.")");
+	
 }
-echo $inserted =  pg_query($dbconn4,"INSERT INTO CUSTOMERS (ID,order_id,first_name,last_name,address,order_amount) VALUES ('6','1006','".$webhook."','test','teste','40')");
-fclose($webhook);
-error_log($webhookContent);
+exit('Query executed!');
 ?>
