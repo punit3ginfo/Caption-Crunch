221517f803c7f7b43e6a54b1a39bb6cad978ddb2
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 18:54:16 +0530

    Update get_datashopify.php

--- get_datashopify.php
+++ get_datashopify.php
@@ -26,7 +26,7 @@ if(!empty($action)){
     $last_name = $data->last_name;
     $address = $data->address->streetaddress;
     $order_amount = $data->total_price;
-	pg_query($dbconn4,"INSERT INTO customers (order_id,first_name,last_name,address,order_amount) VALUES ('{$order_id}','{$first_name}','{$last_name}','{$address}','{$order_amount}')");
+	pg_query($dbconn4,"INSERT INTO customers (order_id,first_name,last_name,address,order_amount) VALUES (2,'{$first_name}','{$last_name}','{$address}',5)");
 	
 }
 
