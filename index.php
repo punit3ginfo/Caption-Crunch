8e72f30a4ed2896503522a09858777259c6a5d92
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 17:06:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "<h1>welcome to my app</h1>";
 require __DIR__.'/connection.php'; 
+require __DIR__.'/conf.php'; 
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 	echo $shop = $_REQUEST['shop'];
