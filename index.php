c61c0696621f799e56f0fd79eb25258e16def20f
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 16:58:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 echo "<h1>welcome to my app</h1>";
 require __DIR__.'/connection.php'; 
-      echo $shop = $_REQUEST['shop'];
-      echo "saad";
-	$shop="testapp-36.myshopify.com";
-
+echo "twutyqwuyiqywi";
+	$shop = $_REQUEST['shop'];
+	$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+	echo "access_token".$access_token."<br>";
 $url = "https://{$shop}/admin/webhooks.json";
 	$topics = array(
 			'customers/create' => "https://shopifyturn2.herokuapp.com/get_datashopify.php
-?action=customer_signup::{$shop}",
-			'orders/create' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_created::{$shop}",
-			'orders/updated' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_updated::{$shop}",
-			'app/uninstalled' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=app_uninstalled::{$shop}",
+?action=customer_signup::{$shop}::{$access_token}",
+			'orders/create' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_created::{$shop}::{$access_token}",
+			'orders/updated' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_updated::{$shop}::{$access_token}",
+			'app/uninstalled' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=app_uninstalled::{$shop}::{$access_token}",
 		);
 	foreach($topics as $topic => $address){
 		$data = array(
