a6a6e1076129072d684e43e1077ab4f0c1c00846
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 15:24:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
 echo "<h1>welcome to my app</h1>";
 require __DIR__.'/connection.php'; 
+	$shop="testapp-36.myshopify.com";
+$url = "https://{$shop}/admin/webhooks.json";
+	$topics = array(
+			'customers/create' => "https://shopifyturn2.herokuapp.com/get_datashopify.php
+?action=customer_signup::{$shop}",
+			'orders/create' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_created::{$shop}",
+			'orders/updated' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=order_updated::{$shop}",
+			'app/uninstalled' => "https://shopifyturn2.herokuapp.com/get_datashopify.php?action=app_uninstalled::{$shop}",
+		);
+	foreach($topics as $topic => $address){
+		$data = array(
+				'access_token' => $access_token,
+				'webhook' => array(
+					'address' => $address,
+					'format' => 'json',
+					'topic' => $topic,
+				)
+			);
+		$data = json_encode($data);	
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');                                                                     
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+			'Content-Type: application/json',                                                                                
+			'Content-Length: ' . strlen($data))                                                                       
+		);
+		curl_exec($ch);
+	}
 	?>
