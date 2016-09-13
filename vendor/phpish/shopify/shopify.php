30edd8e9e8a2662a39b79fc5236ec95d1f7c8105
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-13 16:48:19 +0530

    Update shopify.php

--- vendor/phpish/shopify/shopify.php
+++ vendor/phpish/shopify/shopify.php
@@ -53,10 +53,10 @@
 
 	function access_token($shop, $api_key, $shared_secret, $code)
 	{
-		echo "$shop=".$shop."<br>";
+		/*echo "$shop=".$shop."<br>";
 		echo "$api_key=".$api_key."<br>";
 		echo "$shared_secret=".$shared_secret."<br>";
-		echo "$code".$code."<br>";
+		echo "$code".$code."<br>"; */
 		try
 		{
 			$response = http\request("POST https://$shop/admin/oauth/access_token", array(), array('client_id'=>$api_key, 'client_secret'=>$shared_secret, 'code'=>$code));
