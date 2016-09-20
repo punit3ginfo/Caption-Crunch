9335647cdd0f5821c3bc5faed63c6d72f9c9271e
Nisha3g <nisha.3ginfo@gmail.com>
2016-09-20 18:37:09 +0530

    Update get_datashopify.php

--- get_datashopify.php
+++ get_datashopify.php
@@ -26,28 +26,47 @@ if(!empty($action) &&($action == 'order_created')){
     $last_name = $data1['billing_address']['last_name'];
     $address = $data1['billing_address']['address1'];
     $order_amount = $data1['total_price'];
+    $created_at =$data1['created_at'];
+    $email=$data1['email'];
 	pg_query($dbconn4,"INSERT INTO customers (order_id,first_name,last_name,address,order_amount) VALUES ('{$order_id}','{$first_name}','{$last_name}','{$address}','{$order_amount}')");
 	
-}
+
 
   
 exit('Query executed!');
 
 
 // The data to send to the API
-$postData = array(
-'orderId' => '1516',
-'user.firstName' => 'test',
-'user.lastName' => 'test1',
-'user.emailAddress' => 'jagdeep.32ginfo@gmail.com',
-'items[].titl' => 'testone',
-'items[].url' => 'http://google.com',
-'items[].sku' => '585552'
-);
+$postData='{
+    "orderId": $order_id,
+    "deliveryDate":$created_at,
+    "locale": "en_US",
+    "postalCode": "10667",
+    "emailOptOut": false,
+    "user":          {
+        "firstName":  $first_name,
+        "lastName":  $last_name,
+        "nickName": "",
+        "emailAddress": $email,
+        "externalId": null
+    },
+    "items":     [
+    {
+       "lineItemId": "1",
+       "title": "Sneakers",
+       "url": "www.treadzzzzz.com/sneakers",
+       "sku": "12",
+       "price": "29.99",
+       "itemImageUrl": "www.treadzzzzz.com/img/sneakers.jpg"
+    }
+    
+    ]
+}';
 
+$postDataJson=json_decode($postData);
+// $apple=json_encode($postData);
 // Setup cURL
 $ch = curl_init('https://api.turnto.com/v1/orders/create');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt_array($ch, array(
     CURLOPT_POST => TRUE,
     CURLOPT_RETURNTRANSFER => TRUE,
@@ -55,24 +74,22 @@ curl_setopt_array($ch, array(
         'Authorization: Bearer eAW1zjgK4oZ4HrfCUnJHbWqVfyl6ZUShLkq',
         'Content-Type: application/json;charset=UTF-8'
     ),
-    CURLOPT_POSTFIELDS => json_encode($postData)
+    CURLOPT_POSTFIELDS => json_encode($postDataJson)
 ));
-
 // Send the request
 $response = curl_exec($ch);
-
 // Check for errors
 if($response === FALSE){
     die(curl_error($ch));
+}else{
+	echo "sdfdfsdf";
 }
-
 // Decode the response
 $responseData = json_decode($response, TRUE);
-
 // Print the date from the response
 //echo $responseData;
 echo "<pre>";
 print_r($responseData);
 echo "</pre>";
-
+}
 ?>
