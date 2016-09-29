ff18fc2f4ac9f20ceee38622b7e23d3a1c4cc800
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-29 13:21:52 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-$conn_string = "host=ec2-174-129-223-35.compute-1.amazonaws.com port=5432 dbname=dcdhmp2hbn5ol8 user=iorgedgwzuhxvh password=HtexMOv8jCwSmlLI678h34VFNT";
+$conn_string = "host=ec2-54-221-244-62.compute-1.amazonaws.com port=5432 dbname=dc1l04vbvokmlr user=jihnymieqgdkut password=rhTxZwKWuAKdvqaU21oziua-MX";
 $dbconn4 = pg_connect($conn_string);
 //connect to a database named "dcdhmp2hbn5ol8" on the host "ec2-174-129-223-35.compute-1.amazonaws.com" with a username and password
 if(!$dbconn4){ 
