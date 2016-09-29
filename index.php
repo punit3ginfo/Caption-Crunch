2bd9bba4d3297f153b53f38ce9937e0089e0011f
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-29 13:26:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+// Required File Start.........
+require __DIR__.'/connection.php'; //DB connectivity
+// Required File END...........
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "<h1>welcome to my app</h1>";
