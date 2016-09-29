36e216475c04bde198841b50930b87e59b378a1f
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-29 12:07:11 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
-//adding
-	define('SHOPIFY_APP_API_KEY', 'ecd4248755a8d2d90c9ae1de56646af0');
-	define('SHOPIFY_APP_SHARED_SECRET', 'f4c8a0e9897ca4faa3370549c1d34747');
-// Create the API URL using the private app credentials
-$api_url = 'https://4024250ac11658138b132db0590e0cfe:bf15a7ba9c02040bf26fd59358c42dde@apptest-32.myshopify.com';
+
+	define('SHOPIFY_APP_API_KEY', '917505b6882e263ca3563170982a8a0f');
+	define('SHOPIFY_APP_SHARED_SECRET', 'bd392344caf56ce075f44a18fbc099c8');
+?>
+
