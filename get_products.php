e6dc9f8152ad0bc735a2d0cce594c971c21543e1
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 12:22:14 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -3,11 +3,9 @@
 
 	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
-
 	require __DIR__.'/conf.php';
-
-	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $_SESSION['oauth_token']);
-
+	$access_token = shopify\access_token($_SESSION['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
+	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $access_token);
 	try
 	{
 		# Making an API request can throw an exception
