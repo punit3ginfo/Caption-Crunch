134e2813ac88c8e8ec47390527c4b425167ac01a
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 12:23:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,32 +5,7 @@ require __DIR__.'/connection.php'; //DB connectivity
 // Required File END...........
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo "<h1>welcome to my app12</h1>";
-	require __DIR__.'/vendor/autoload.php';
-	use phpish\shopify;
-	require __DIR__.'/conf.php';
-$access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
-	$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token);
-	try
-	{
-		# Making an API request can throw an exception
-		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
-		print_r($products);
-	}
-	catch (shopify\ApiException $e)
-	{
-		# HTTP status code was >= 400 or response contained the key 'errors'
-		echo $e;
-		print_r($e->getRequest());
-		print_r($e->getResponse());
-	}
-	catch (shopify\CurlException $e)
-	{
-		# cURL error
-		echo $e;
-		print_r($e->getRequest());
-		print_r($e->getResponse());
-	}
-
+echo "<h1>welcome to my app79</h1>";
+require __DIR__.'/get_products.php'; //GET PRODUCTS
 ?>
 
