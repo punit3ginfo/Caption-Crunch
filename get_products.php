94b451a8a4147cd8c38295226b13b47b0424d7d7
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 18:10:17 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -10,23 +10,6 @@
 	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
 	require __DIR__.'/conf.php';
-
-	 var Shopify = new shopifyAPI({
-                 shop: 'SHOP_NAME', 
-                 shopify_api_key: 'SHOPIFY_APP_API_KEY', // Your API key
-                 shopify_shared_secret: 'API_SHARED_SECRET'// Your Shared Secret
-             });
-
-// More secure if we get the user email through the server
-var activeUserEmail         = getCurentUser.email;
-var findCustomerByEmailURL  = "/admin/customers/search.json?query=" + activeUserEmail;
-
-Shopify.get(findCustomerByEmailURL, function(err, data, headers){
-     console.log(data); // Data contains customer json information
-
- });`
-
-
 	$_SESSION['shop']=$_REQUEST['shop'];
 	$access_token = shopify\access_token($_SESSION['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
 	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $access_token);
@@ -36,59 +19,8 @@ Shopify.get(findCustomerByEmailURL, function(err, data, headers){
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
 		$products_1 = $shopify('PUT /admin/products/#7885330952.json', array('published_status'=>'published'));
 		print_r($products_1);exit;
-		//$update = $shopify('PUT', "/admin/products/7885327240.json", $product); 	
-		
-		$arrayDateAndMachine['product'] = array();
-		$arrayDateAndMachine['id']='7885330952';
-		$arrayDateAndMachine['tags']='Barnes';
-
-		
-$ch = curl_init();
-		
-$headers = array(
-    'Accept: application/json',
-    'Content-Type: application/json',
-);		
-		
-// URL for curl
-$url = "https://share-tag.myshopify.com/admin/products/#7885330952.json";
-
-// Clean up string
-$putString = stripslashes($arrayDateAndMachine['product']);
-// Put string into a temporary file
-
-
-// Headers
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// Binary transfer i.e. --data-BINARY
-curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
-// Using a PUT method i.e. -XPUT
-curl_setopt($ch, CURLOPT_PUT, true);
-
-
-
-$output = curl_exec($ch);
- var_dump($output);exit;
-		
-
-// Close the file
-fclose($putData);
-// Stop curl
-curl_close($ch); 
-		
-		
-		
-		
-		
-		
 		foreach($products as $singleproduct)
 		{
-			
-		
-			
-		
 			$title=$singleproduct['title'];
 			$variants=$singleproduct['variants'];
 			foreach($variants as $variants){
