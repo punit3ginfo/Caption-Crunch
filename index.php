67184556b6f267fe3f54f6885048a37f43b66e37
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 19:44:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__.'/connection.php'; //DB connectivity
 // Required File END...........
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo "<h1>welcome to my app79</h1>";
+echo "<h1>welcome to my app</h1>";
 require __DIR__.'/get_products.php'; //GET PRODUCTS
 ?>
 
