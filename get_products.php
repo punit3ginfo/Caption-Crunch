ec28d3e6901c17f426392c6408d2a0275d16006c
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 17:23:59 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -70,7 +70,7 @@ array('id' => '321',
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
 		
 		
-		echo $baseUrl="$shopify('https://share-tag.myshopify.com//admin/products/7885330952.json');
+		echo $baseUrl=$shopify('https://share-tag.myshopify.com//admin/products/7885330952.json');
 	$ch = curl_init($baseUrl);  //note product ID in url
 echo $data_string = json_encode(array('product'=>$product)); //json encode the product array
 		
