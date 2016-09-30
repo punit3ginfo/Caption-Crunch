0bdbd47418697c238f9fc90c12929bca592041dc
punit3ginfo <punit.3ginfo@gmail.com>
2016-09-30 17:47:15 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -17,8 +17,54 @@
 	{
 		# Making an API request can throw an exception
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		echo $products_1 = $shopify('PUT /admin/products/#7885330952.json', array('published_status'=>'published'));
 		
-		//$update = $shopify('PUT', "/admin/products/7885327240.json", $product); 		
+		//$update = $shopify('PUT', "/admin/products/7885327240.json", $product); 	
+		
+		// Start curl
+		$query={
+  "product": {
+    "id": 7885330952,
+    "tags": "Barnes & Noble, John's Fav"
+  }
+};
+$ch = curl_init();
+// URL for curl
+$url = "https://share-tag.myshopify.com/admin/products/#7885330952.json";
+
+// Clean up string
+$putString = stripslashes($query);
+// Put string into a temporary file
+$putData = tmpfile();
+// Write the string to the temporary file
+fwrite($putData, $putString);
+// Move back to the beginning of the file
+fseek($putData, 0);
+
+// Headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// Binary transfer i.e. --data-BINARY
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+// Using a PUT method i.e. -XPUT
+curl_setopt($ch, CURLOPT_PUT, true);
+// Instead of POST fields use these settings
+curl_setopt($ch, CURLOPT_INFILE, $putData);
+curl_setopt($ch, CURLOPT_INFILESIZE, strlen($putString));
+
+$output = curl_exec($ch);
+echo $output;
+
+// Close the file
+fclose($putData);
+// Stop curl
+curl_close($ch); 
+		
+		
+		
+		
+		
 		
 		foreach($products as $singleproduct)
 		{
