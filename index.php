179b493d56427532b64fcef75b6728c1e89aa458
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 15:52:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ ini_set('display_errors', 1);
   <title>Caption Crunch</title>
   <link rel="stylesheet" href="style.css">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/core.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
 </head>
 
 <body>
