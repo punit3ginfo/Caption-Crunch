52bdbfafc69b7f0134ecd7ee82450e0df37c0013
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 16:50:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 // Required File Start.........
 require __DIR__.'/connection.php'; //DB connectivity
 // Required File END...........
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
- echo "<h1>SUHHHHHHHH</h1>";
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 // require __DIR__.'/get_products.php'; //GET PRODUCTS
 ?>
 
