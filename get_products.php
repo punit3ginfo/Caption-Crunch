5cfc29fbf19e401d2ca8dcfbf070635d75944c0a
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 12:11:21 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -6,11 +6,51 @@
 	require __DIR__.'/conf.php'; //Configuration
 	$_SESSION['shop']=$_REQUEST['shop'];
 	$access_token = shopify\access_token($_SESSION['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_REQUEST['code']);
-	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $access_token);	
+	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $access_token);
+
+$baseUrl = 'https://917505b6882e263ca3563170982a8a0f:c91ad52cc8ae78a4c005a1240221652b@share-tag.myshopify.com/admin/';
+$product =
+array('id' => '7885330952',
+'title' => 'My New Product',
+'body_html' => 'My New Product Description',
+'vendor'=> 'My Product Vendor',
+'product_type'=> 'My Product Type',
+);
+
+$ch = curl_init($baseUrl.'products/123.json');  //note product ID in url
+$data_string = json_encode(array('product'=>$product)); //json encode the product array
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  //specify the PUT verb for update
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  //add the data string for the request
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //set return as string true
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+'Content-Type: application/json',
+'Content-Length: ' . strlen($data_string))
+); //set the header as JSON
+$server_output = curl_exec ($ch); //execute and store server output
+
+print_r($server_output);
+exit();
 	try
 	{
 		# Making an API request can throw an exception
 		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		
+		/* Share Tag Code Satrt */
+	
+		
+		
+	
+
+    // Call our Shopify function
+    //$shopify_response = shopify_call(NULL, $shop, "/admin/oauth/access_token", $query, 'POST');
+		
+		/* Share Tag Code END */
+		
+		
+		
+		
+		
+		
 		foreach($products as $singleproduct)
 		{
 			$title=$singleproduct['title']; // Product Title
