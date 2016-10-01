99b93043b5733d7b7271a651135a196b3801c17b
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 15:21:50 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -112,3 +112,51 @@ $server_output = curl_exec ($ch); */   //execute and store server output
 		print_r($e->getRequest());
 		print_r($e->getResponse());
 	}
+
+try
+	{
+		echo "<script>alert(2);</script>";
+		# Making an API request can throw an exception
+		$product = $shopify('POST /admin/products.json', array(), array
+		(
+			'product' => array
+			(
+				"title" => "Burton Custom Freestlye 151",
+				"body_html" => "<strong>Good snowboard!</strong>",
+				"vendor" => "Burton",
+				"product_type" => "Snowboard",
+				"variants" => array
+				(
+					array
+					(
+						"option1" => "First",
+						"price" => "10.00",
+						"sku" => 123,
+					),
+					array (
+						"option1" => "Second",
+						"price" => "20.00",
+						"sku" => "123"
+					)
+				)
+			)
+		));
+
+		print_r($product);
+	}
+	catch (shopify\ApiException $e)
+	{
+		echo "<script>alert(3);</script>";
+		# HTTP status code was >= 400 or response contained the key 'errors'
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
+	catch (shopify\CurlException $e)
+	{
+		echo "<script>alert(4);</script>";
+		# cURL error
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
