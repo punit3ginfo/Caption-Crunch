7ca517de4d4177c054cad80b4699381da138b7e2
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 12:08:17 +0530

    Update shopify.php

--- vendor/phpish/shopify/shopify.php
+++ vendor/phpish/shopify/shopify.php
@@ -54,11 +54,7 @@
 	function access_token($shop, $api_key, $shared_secret, $code)
 	{
 		
-		echo "code=".$code;
-		echo "shared_secret=".$shared_secret;
 		
-		
-		exit;
 		/*echo "$shop=".$shop."<br>";
 		echo "$api_key=".$api_key."<br>";
 		echo "$shared_secret=".$shared_secret."<br>";
