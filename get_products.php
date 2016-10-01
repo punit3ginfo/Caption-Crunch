620e237b4e6fcde814ee57e0abab2f70bd044dc7
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-01 16:23:03 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -115,11 +115,11 @@ $server_output = curl_exec ($ch); */   //execute and store server output
 	?>
 	<script>
 	function shareButton(pid){
-               var code='<?php echo $_REQUEST['code'] ?>';
+               var access_token='<?php echo $access_token ?>';
 		var shop='<?php echo $_REQUEST['shop'] ?>';
 		
                 $.ajax({
-                    url: '/sharebutton.php?pid='+ pid+'&code='+code+'&shop='+shop,
+                    url: '/sharebutton.php?pid='+ pid+'&access_token='+access_token+'&shop='+shop,
                     success: function(data){
                        alert("yahoooooooooo");
                     }
