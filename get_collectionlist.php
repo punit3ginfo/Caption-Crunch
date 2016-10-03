9fd8a411001b5eef9c63d47b392cb714b42d0187
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 18:29:42 +0530

    Create get_collectionlist.php

---
+++ get_collectionlist.php
@@ -0,0 +1,27 @@
+<?php 
+     require __DIR__.'/vendor/autoload.php';
+     use phpish\shopify;
+     $access_token=$_REQUEST['access_token'];
+     $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token ); 
+   try
+	{
+		# Making an API request can throw an exception
+		$collections = $shopify('GET /admin/collects.json', array('published_status'=>'published'));
+		print_r($collections);
+		
+	}
+	catch (shopify\ApiException $e)
+	{
+		# HTTP status code was >= 400 or response contained the key 'errors'
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
+	catch (shopify\CurlException $e)
+	{
+		# cURL error
+		echo $e;
+		print_r($e->getRequest());
+		print_r($e->getResponse());
+	}
+	?>
