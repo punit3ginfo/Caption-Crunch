c6e7404a02bde8f1c42d9a8ee702c6db7b438de0
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 19:11:05 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -8,33 +8,18 @@
 ?>
 
 <?php
-/* Share Tag Code Satrt ------------------------------------------------------------------------*/
-/*
-$baseUrl = 'https://23c709fbea4a1fc65b1e6a54a10d430c:4f36a56e3e52c00978d6dda49d23a318@share-tag.myshopify.com/admin/';
-$product =
-array(
- "tags"=> "share"
-);
 
-$ch = curl_init($baseUrl.'products/7885327240.json');  //note product ID in url
-$data_string = json_encode(array('product'=>$product)); //json encode the product array
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  //specify the PUT verb for update
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  //add the data string for the request
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //set return as string true
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-'Content-Type: application/json',
-'Content-Length: ' . strlen($data_string))
-); //set the header as JSON 
-$server_output = curl_exec ($ch); */   //execute and store server output
-
-
-/* Share Tag Code END -----------------------------------------------------------------------------*/
 
 
 	try
 	{
 		# Making an API request can throw an exception
-		$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		if(isset($_REQUEST['colid']) && $_REQUEST['colid']!=''){
+		$products = $shopify('GET /admin/products.json?collection_id={$_REQUEST['colid']}', array('published_status'=>'published'));
+		}
+		else {
+	       $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		}
 		//print_r($products);
 		foreach($products as $singleproduct)
 		{
