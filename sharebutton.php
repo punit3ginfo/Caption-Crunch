c4016dfda422d2c56066bfe59fe7674cbdd91db5
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 17:00:15 +0530

    Update sharebutton.php

--- sharebutton.php
+++ sharebutton.php
@@ -7,7 +7,6 @@ session_start();
 	require __DIR__.'/conf.php'; //Configuration
 	$_SESSION['shop']="share-tag.myshopify.com";
         $pid=$_REQUEST['pid'];
-        
  	$access_token=$_REQUEST['access_token'];
 	$shop=$_REQUEST['shop'];
 	//$access_token = shopify\access_token($shop, SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $code);
@@ -33,7 +32,7 @@ try
 	}
 	catch (shopify\ApiException $e)
 	{
-		echo "<script>alert(3);</script>";
+		
 		# HTTP status code was >= 400 or response contained the key 'errors'
 		echo $e;
 		print_r($e->getRequest());
