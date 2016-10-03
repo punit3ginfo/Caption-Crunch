c0de6ab595cca353cda046671ccd82f2c81c9bc7
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 12:16:55 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,5 +1,5 @@
 <?php
-        
+        echo "fifth part1235";
 	define('SHOPIFY_APP_API_KEY', '917505b6882e263ca3563170982a8a0f');
 	define('SHOPIFY_APP_SHARED_SECRET', 'bd392344caf56ce075f44a18fbc099c8');
 	require __DIR__.'/vendor/autoload.php';
