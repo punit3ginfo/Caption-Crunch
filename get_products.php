4116277251c2878764f338c9b43f6addf7a4d908
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 12:20:22 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -2,8 +2,8 @@
 	
          echo "first part123";
 	require __DIR__.'/conf.php'; //Configuration
-      
-        
+      $access_token=$_REQUEST['access_token'];
+        $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token); 
 echo "123";
 ?>
 
