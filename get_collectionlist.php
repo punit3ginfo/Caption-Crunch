c049ec6e242e4f62b775342a907b293d80d21c68
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 18:44:15 +0530

    Update get_collectionlist.php

--- get_collectionlist.php
+++ get_collectionlist.php
@@ -7,8 +7,30 @@
 	{
 		# Making an API request can throw an exception
 		$collections = $shopify('GET /admin/custom_collections.json', array('published_status'=>'published'));
+
 		print_r($collections);
-		
+		foreach($collections as $singlecollection)
+		{
+			$title=$singlecollection['title']; // collection Title
+			$id=$singlecollection['id']; // collection id
+			$handle=$singlecollection['handle']; // collection id ?>
+			
+			<a href="<?php echo $handle;  ?>">                 
+                                    <div class="chat_select_container">
+                                        <div class="chat_personal_info_container">
+                                            <div class="chat_user_info">
+                                                <span class="collection_list_text"><?php echo $title;  ?></span>
+                                            </div>
+                                            <div class="notification-container">
+                                                <div class="chat_notification_container">
+                                                    <span class="notif_number">354</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+								
+		<?php }
 	}
 	catch (shopify\ApiException $e)
 	{
