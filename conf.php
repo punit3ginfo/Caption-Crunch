7d9ee3e283ab837914701a1b8d8e134ab175a7cb
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-03 12:09:52 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -2,13 +2,16 @@
         
 	define('SHOPIFY_APP_API_KEY', '917505b6882e263ca3563170982a8a0f');
 	define('SHOPIFY_APP_SHARED_SECRET', 'bd392344caf56ce075f44a18fbc099c8');
+	require __DIR__.'/vendor/autoload.php';
+        use phpish\shopify;
        echo "a=".$_SESSION['shop']=$_REQUEST['shop'];
 		$_SESSION['code']=$_REQUEST['code'];
 		echo '<pre>';
 		print_r($_SESSION);
 		echo '</pre>';
            echo $_SESSION['shop'];
-         echo "fifth part";
+         echo "fifth part123";
+
 	echo "b=".$access_token = shopify\access_token($_SESSION['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_SESSION['code']);
 echo "dfsgfdgsfdg";	
 echo "c=".$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $access_token); 
