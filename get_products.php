1e90964f35d2926617e8807230e2843c754185a1
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-05 16:18:17 -0700

    Update

--- get_products.php
+++ get_products.php
@@ -26,8 +26,8 @@ try
 		        $p_id1=$singleproduct['id'];
 		        $tags=$singleproduct['tags'];
 			   $OrigonalTag=$singleproduct['tags'];
-			
-			
+
+
 		        $tags = str_replace('shared', '', $tags);
 				$tags = str_replace(' ', '', $tags);
 			    $tags = str_replace(',', 'AA', $tags);
@@ -59,52 +59,53 @@ try
       			<div class="product-card-details-container">
                   	<span class="product-title-text"><?php echo $title; ?></span>
                   	<div class="product-card-price-container">
-                  		<span class="product-card-price-text" style="margin-right: 3px;">$<?php echo $price; ?></span><span class="product-card-price-text" style="font-size: 14px; color: #888;">$<?php echo $price; ?></span>
+                  		<span class="product-card-price-text" style="margin-right: 3px;">$<?php echo $price; ?></span>
+                      <span class="product-card-price-text" style="font-size: 14px; color: #888;">$<?php echo $price; ?></span>
                   	</div>
       			</div>
       		</div>
-		
-		
-		
-		
-		
+
+
+
+
+
             <div id="<?php echo $p_id1; ?>"  class="share-button-container?>" >
-		    
-		    
+
+
 			<script>
 	$(document).ready(function(){
 		var OrigonalTag = '<?php echo $OrigonalTag; ?>';
-		
+
 		var pid_1 = '<?php echo $p_id1; ?>';
 		//alert(pid_1);
 		var pattern = /shared/;
 		var pattern1 = / shared/;
-		
+
 		var exists = pattern.test(OrigonalTag);
 		var exists1 = pattern1.test(OrigonalTag);
 		if(exists || exists1 ){
 			var tags_1 = '"<?php echo $tags; ?>"';
 			//alert(tags_1);
-			 
+
 		        var _id = '#'+ pid_1;
 
-			     
+
 		//$(_id).html('<button type=button class=share-button onclick=unshareButton('+pid_1+',"'+tags_1+'");>UnShare</button>');
 		$(_id).html("<button type='button' class='share-button' onclick='unshareButton("+pid_1+","+tags_1+");'>UnShare</button>");
 
 		}else{
 			var _id = '#'+ pid_1;
-			<?php 
+			<?php
 			if($OrigonalTag == '')
 			{
-			  $OrigonalTag="shared";	
+			  $OrigonalTag="shared";
 			}
 			?>
 			var tags_1 = '<?php echo $OrigonalTag; ?>';
-			   
+
 			  $(_id).html('<button type="button" class=share-button onclick=shareButton('+pid_1+',"'+tags_1+'");>Share</button>');
 	}
-	});   
+	});
 </script>
 	   </div>
       </div>
@@ -131,11 +132,11 @@ try
 	?>
 
  <?php
-		        
+
 	       ?>
 	<script>
 	function shareButton(pid,tags){
-               
+
                var access_token='<?php echo $access_token ?>';
 	       var shop='<?php echo $_REQUEST['shop'] ?>';
                var tags_unshare = tags.replace('shared', "");
@@ -153,7 +154,7 @@ try
     <script>
 
 	var tags;
-	function unshareButton(pid,tags){    
+	function unshareButton(pid,tags){
 		var _id = '#'+ pid;
                 var access_token='<?php echo $access_token ?>';
 	        var shop='<?php echo $_REQUEST['shop'] ?>';
@@ -164,8 +165,8 @@ try
 
                 $.ajax({
                     url: '/sharebutton.php?pid='+ pid+'&access_token='+access_token+'&shop='+shop+'&tags='+tags,
-                    success: function(data){		     	
-			    
+                    success: function(data){
+
 			  $(_id).html('<button type="button" class=share-button def onclick=shareButton('+pid+',"'+tags_1+'");>Share</button>');
                     }
                 });
