ba55759a18e9012468f209344adcf555b2399038
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-05 18:21:34 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -83,7 +83,7 @@ try
 		var exists = pattern.test(OrigonalTag);
 		var exists1 = pattern1.test(OrigonalTag);
 		if(exists || exists1 ){
-			var tags_1 = '<?php echo "'".$tags."'"; ?>';
+			var tags_1 = <?php echo "'"; echo $tags; echo "'"; ?>;
 			alert(tags_1);
 			 
 		        var _id = '#'+ pid_1;
