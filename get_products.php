47709afb7a22a8cfcdc7f6ad0aa1e483e0652078
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-05 18:12:39 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -90,7 +90,7 @@ try
 
 			     
 		//$(_id).html('<button type=button class=share-button onclick=unshareButton('+pid_1+',"'+tags_1+'");>UnShare</button>');
-		$(_id).html("<button type='button' class='share-button' onclick='unshareButton("+pid_1+","+tags_1+");'>UnShare</button>");
+		$(_id).html("<button type='button' class='share-button' onclick='unshareButton("+pid_1+",'"+tags_1+"');'>UnShare</button>");
 
 		}else{
 			var _id = '#'+ pid_1;
@@ -171,7 +171,3 @@ try
                 });
             }
     </script>
-
-
-
-
