49d6ca34757f29edcb99795599e324f1677dd263
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-06 18:20:48 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -16,7 +16,9 @@ try
 		}
 		if($_REQUEST['colid']=='' && $_REQUEST['status']=='') {
 			
-	       $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+	      // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+			$products = $shopify('GET /admin/products.json?title=fgdffsfd', array('published_status'=>'published'));
+			
 		}
 		
 		
