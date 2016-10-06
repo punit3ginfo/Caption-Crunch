63aba7fbfdcb64293a6cb5f8a890a0a2a927dfe0
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-05 21:17:50 -0700

    Update

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,43 @@
 <div class="content-container">
-  <?php
-  echo "Dashboard";
-  ?>
+
+  <div class="chat_container" style="overflow: hidden;">
+  			 <div class="collection-sidebar-container">
+                		<div class="collection_title_selector">
+                              <div class="collection_title_container">
+                                  <span class="collection_title_text">Dashboard</span>
+                              </div>
+                      </div>
+
+                  	<div class="chat_selector_overflow_container">
+                          <div class="chat_selector_overflow">
+                            		<a href="/#">
+                                      <div class="chat_select_container" >
+                                          <div class="chat_personal_info_container">
+                                              <div class="chat_user_info">
+                                                  <span class="collection_list_text">Feed</span>
+                                              </div>
+                                          </div>
+                                      </div>
+                                </a>
+                          </div>
+                    	</div>
+                </div>
+
+
+  			<div class="collection_title_header">
+                   <div class="collection_header_container">
+                       <div class="caption-header-container">
+                         	  <div class="builder-title-header-container" style=" width: 100%;">
+
+  						              </div>
+                       </div>
+                    </div>
+          	</div>
+
+  			<div class="product-grid-container">
+          		<div  class="product-grid-overflow-container">
+
+      			</div>
+    			</div>
+  </div>
 </div>
