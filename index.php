6eee66c074c012777171d6813f2efe6a4d030e78
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-06 12:03:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -208,7 +208,7 @@ function gethistory(){
 		var shop='<?php echo $_REQUEST['shop'] ?>';
 
                 $.ajax({
-                    url: '/history.php?access_token='+access_token+'&shop='+shop+'&status=history',
+                    url: '/collections.php?access_token='+access_token+'&shop='+shop+'&status=history',
                     success: function(data){
                      //console.log(data);
 			   // var data1= data.find('.chat_container').html()
