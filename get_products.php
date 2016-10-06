0176577cb978401b537be2174c0980353ecd0974
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-06 14:59:41 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -19,8 +19,8 @@ try
 		}
 		if($_REQUEST['status']=="history")
 		{
-			echo "<script>alert(78);</script>";
-			$products = $shopify('GET /admin/products.json?tags=shared', array('published_status'=>'published'));
+			echo "<script>alert(781);</script>";
+			$products = $shopify('GET /admin/products.json?title="7X Felt Snapback in Gray"', array('published_status'=>'published'));
 		}
 		//print_r($products);
 		//$products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
