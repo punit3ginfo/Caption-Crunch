1e8879dd6d60e1d93027cc309e65249aaf808f8a
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-07 12:53:27 +0530

    Update get_collectionlist.php

--- get_collectionlist.php
+++ get_collectionlist.php
@@ -76,4 +76,24 @@
             }
 	</script>
 
+	
+<script>
+	function search(p_title){
+                
+               var access_token='<?php echo $access_token ?>';
+		var shop='<?php echo $_REQUEST['shop'] ?>';
+                var p_title= '"'+p_title+'"';
+
+                $.ajax({
+                    url: '/history.php?access_token='+access_token+'&shop='+shop+'&title='+p_title+'&status=search',
+                    success: function(data){
+                     //console.log(data);
+			   // var data1= data.find('.chat_container').html()
+			    $('.product-grid-container').html(data);
+                    }
+                });
+            }
+</script>	
+	
+
 
