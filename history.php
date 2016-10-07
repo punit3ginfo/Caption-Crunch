8d4bbc6f8c93da8e793a6c444d1e3a2eceaae2ba
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-07 12:56:00 +0530

    Update history.php

--- history.php
+++ history.php
@@ -70,84 +70,7 @@ try
 			}
             
 			?>
-
-<div class="chat_container" style="overflow: hidden;">
-  <div class="collection-sidebar-container">
-              		<div class="collection_title_selector">
-                            <div class="collection_title_container">
-                                <span class="collection_title_text">Collections</span>
-                            </div>
-                    </div>
-
-                	<div class="chat_selector_overflow_container">
-                        <div class="chat_selector_overflow">
-
-                          	<!-- Collection Card -->
-
-
-                                <?php
-
-       				// echo "Product's go here"
-       				require __DIR__.'/get_collectionlist.php'; //GET PRODUCTS
-        				?>
-
-                             <!-- Collection Card -->
-
-                        </div>
-                  	</div>
-              </div>
-
-
-
-
-						<div class="collection_title_header">
-
-                          <div class="collection-share-history-container">
-                            <a href="javascript:void(0)" onclick="gethistory()" class="sidebar-span share-history-button">
-				    <i class="fa fa-history" aria-hidden="true"></i> 
-				    Share History
-			    </a>
-                          </div>
-                          <div class="collection_header_container">
-
-                            <div class="pagination-clearfix">
-
-                                  <div class="grid-item pagination-border-top">
-                                    <div class="grid">
-                                      <div class="grid-item">
-                                          <div class="text-center">
-                                            <?php
-                                            echo "Pagination goes here";
-                                            ?>
-                                          </div>
-                                      </div>
-                                    </div>
-                                  </div>
-
-                          	</div>
-
-                            <div class="collection_title_header_container">
-                               <div class="collection_title_container-active">
-                                 <form class="search-form" method="">
-                                    <input class="search-text-box" id="formValueId" type="text" name="q_name" placeholder="Search" />
-                                    <input class="search-icon-button" type="button" onclick="search(document.getElementById('formValueId').value);" value="&#xf002;" />
-                                </form>
-                               </div>
-                            </div>
-
-                          </div>
-        				</div>
 		
-	
-	
-	
-
-  <div class="product-grid-container scroll-grid">
-      <div  class="product-grid-overflow-container">
-        
-	
-			
-
 			
 <!-- HTML Content for Product  START      -->
 
@@ -216,9 +139,7 @@ try
 <!-- HTML Content for Product END    -->
 
 
- </div>
-  </div>
-</div>
+
 
 
 
