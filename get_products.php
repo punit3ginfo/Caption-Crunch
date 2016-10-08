149627f7a7aaca7c1c2a8c887a8673dc77f0843a
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-08 14:43:39 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -24,7 +24,7 @@ try
 		if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) ) {
 
 	     // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
-		$products = $shopify('GET /admin/products.json', array('limit'=>'12','page'=>'2'));
+		$products = $shopify('GET /admin/products.json', array('limit'=>'12','page'=>$_REQUEST['page_id']));
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
