0758adc23e385dd21005d7565743347d1f595268
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-08 12:22:07 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -1,9 +1,4 @@
-<?php 
-	echo "welcome";
-    echo "count".$item_per_page="<script>var number=document.getElementById('product_count').value; alert(number);</script>";
-	$item_per_page=5;
-	echo "no_of_pages=".$pages = ceil($item_per_page/$item_per_page);	
-?>
+
 
 <div class="chat_container" style="overflow: hidden;">
   <div class="collection-sidebar-container">
@@ -87,6 +82,13 @@
 		 require __DIR__.'/get_products.php';
 	}
         ?>
+	      <?php 
+	echo "welcome";
+    echo "count".$item_per_page="<script>var number=document.getElementById('product_count').value; alert(number);</script>";
+	$item_per_page=5;
+	echo "no_of_pages=".$pages = ceil($item_per_page/$item_per_page);	
+?>
+	      
       </div>
   </div>
 </div>
