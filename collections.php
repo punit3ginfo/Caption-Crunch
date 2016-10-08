a180d949c88a723dfa5fccecfc195525e6eb7a05
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-08 13:39:41 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -54,6 +54,8 @@
 						  <ul id="pagination-list">
 							  <li onclick="getPaging(this.id)" id="1">1</li>
 							<li onclick="getPaging(this.id)" id="2">2</li>
+							  <li onclick="getPaging(this.id)" id="3">3</li>
+							<li onclick="getPaging(this.id)" id="4">4</li>
 						   </ul>
                                             <?php
                                             //echo "Pagination goes here";
