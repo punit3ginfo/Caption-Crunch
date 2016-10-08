02c163274622e2ae57f6f41f04e846a5bc7db8d8
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-08 14:50:38 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -11,9 +11,12 @@
 try
 	{
 		# Making an API request can throw an exception
-		if(isset($_REQUEST['colid']) && $_REQUEST['colid']!='' && $_REQUEST['status']==''){
+		if(isset($_REQUEST['colid']) && $_REQUEST['colid']!='' && $_REQUEST['status']=='' && isset($_REQUEST['page_id'])){
+			
+				
 		$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('published_status'=>'published','limit'=>'12','page'=>$_REQUEST['page_id'])) );
-		}
+			
+			}
 		if($_REQUEST['colid']=='' && $_REQUEST['status']=='' && !isset($_REQUEST['page_id']) ) {
 
 	     // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
