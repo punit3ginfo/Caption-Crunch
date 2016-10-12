6c1dd0a02910ba56c51a78711bbad21b0fdc6256
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-12 17:42:55 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -25,13 +25,18 @@ try
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
-		if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) ) {
+		if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) && !(isset($_REQUEST['colid'])) {
 
 	     // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
 		$products = $shopify('GET /admin/products.json', array('limit'=>$_REQUEST['limit'],'page'=>$_REQUEST['page_id']));
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
+		  if(isset($_REQUEST['colid']) && $_REQUEST['colid']!=''&&  isset($_REQUEST['page_id']))
+		   {
+			$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('published_status'=>'published',$_REQUEST['page_id']));
+		}
+		   
 
 
 
