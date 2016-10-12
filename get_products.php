1378071ec4fdcd23b3a154bca59178c4da84d20a
punit3ginfo <punit.3ginfo@gmail.com>
2016-10-12 18:12:40 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -25,10 +25,10 @@ try
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
-		if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) && !(isset($_REQUEST['colid']))) {
+		if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) && (isset($_REQUEST['colid']))) {
                 echo "4";
 	     // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
-		$products = $shopify('GET /admin/products.json', array('limit'=>$_REQUEST['limit'],'page'=>$_REQUEST['page_id']));
+		$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('limit'=>$_REQUEST['limit'],'page'=>$_REQUEST['page_id']));
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
