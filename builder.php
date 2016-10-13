c6d3f6d08a1edd1d079bd98b7350597d7937dc29
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-12 18:30:46 -0700

    Update

--- builder.php
+++ builder.php
@@ -184,7 +184,7 @@
 																						</div>
 																				</div>
 
-																				<div class="feed-product-main-conatiner">
+																				<div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
 																						<div class="feed-product-main-clearfix">
 																										<div class="feed-product-image" style="background: #65D35B;">
 																											<div class="feed-product-image-zoom" style="background: #65D35B;">
@@ -201,6 +201,57 @@
 																						</div>
 																				</div>
 
+																				<div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
+																						<div class="feed-product-main-clearfix">
+																										<div class="feed-product-image" style="background: #65D35B;">
+																											<div class="feed-product-image-zoom" style="background: #65D35B;">
+
+																											</div>
+																										</div>
+																										<div class="feed-product-titledate-container">
+																														<span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
+																										</div>
+																										<div class="feed-product-sharenow-container">
+																												<a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
+																												<button type='button' class='feed-product-sharenow-button'><i class="fa fa-share" aria-hidden="true"></i> Share</button>
+																										</div>
+																						</div>
+																				</div>
+
+																				<div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
+																						<div class="feed-product-main-clearfix">
+																										<div class="feed-product-image" style="background: #65D35B;">
+																											<div class="feed-product-image-zoom" style="background: #65D35B;">
+
+																											</div>
+																										</div>
+																										<div class="feed-product-titledate-container">
+																														<span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
+																										</div>
+																										<div class="feed-product-sharenow-container">
+																												<a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
+																												<button type='button' class='feed-product-sharenow-button'><i class="fa fa-share" aria-hidden="true"></i> Share</button>
+																										</div>
+																						</div>
+																				</div>
+
+																				<div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
+																						<div class="feed-product-main-clearfix">
+																										<div class="feed-product-image" style="background: #65D35B;">
+																											<div class="feed-product-image-zoom" style="background: #65D35B;">
+
+																											</div>
+																										</div>
+																										<div class="feed-product-titledate-container">
+																														<span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
+																										</div>
+																										<div class="feed-product-sharenow-container">
+																												<a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
+																												<button type='button' class='feed-product-sharenow-button'><i class="fa fa-share" aria-hidden="true"></i> Share</button>
+																										</div>
+																						</div>
+																				</div>
+																				
 																				<!-- Product List End -->
 													 						</div>
 													 			</div>
