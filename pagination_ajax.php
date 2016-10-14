f06b9375e5f053a7d8a8f4803829727f932620cd
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-14 10:43:27 -0700

    Update

--- pagination_ajax.php
+++ pagination_ajax.php
@@ -8,52 +8,52 @@
 try
 	{
 		if(isset($_REQUEST['colid']) && $_REQUEST['colid']!='' && $_REQUEST['status']==''){
-			
-			           
+
+
 						   $TotalnoOfProduct = $shopify('GET /admin/products/count.json', array('collection_id'=>$_REQUEST['colid']));
 							//echo "total products=".$TotalnoOfProduct=sizeof($products);
-							$limit=50; // Number of product per page
+							$limit=21; // Number of product per page
 							$noofPages=$TotalnoOfProduct/$limit;
 							$noofPages=abs(round($noofPages));
 			?>
 			<button type="button" id="showLess"><i class="fa fa-long-arrow-left"></i></button> <ul id="pagination-list">
-												 
+
 						  <?php
 						     for($i=1;$i<=$noofPages;$i++)
 							 {?>
-								<li class="page-link" onclick="getPaging(this.id,<?php echo $limit; ?>,<?php echo $_REQUEST['colid']; ?>)" 
+								<li class="page-link" onclick="getPaging(this.id,<?php echo $limit; ?>,<?php echo $_REQUEST['colid']; ?>)"
 								    id="<?php echo $i; ?>"><?php echo $i; ?></li>
-								
+
 						  <?php } ?>
-												  
-								
+
+
 						  </ul>
 						   <button type="button" id="loadMore"><i class="fa fa-long-arrow-right"></i></button>
-			
-		<?php	
+
+		<?php
 		}else{
-			
+
 							$TotalnoOfProduct = $shopify('GET /admin/products/count.json');
 							//echo "total products=".$TotalnoOfProduct=sizeof($products);
 							$limit=50; // Number of product per page
 							$noofPages=$TotalnoOfProduct/$limit;
 							$noofPages=abs(round($noofPages));
-		
-		?>				
-						 
+
+		?>
+
 						<button type="button" id="showLess"><i class="fa fa-long-arrow-left"></i></button> <ul id="pagination-list">
-												 
+
 						  <?php
 						     for($i=1;$i<=$noofPages;$i++)
 							 {?>
 								<li class="page-link" onclick="getPagingALLProduct(this.id,<?php echo $limit; ?>)" id="<?php echo $i; ?>"><?php echo $i; ?></li>
-								
+
 						  <?php } ?>
-												  
-								
+
+
 						  </ul>
 						   <button type="button" id="loadMore"><i class="fa fa-long-arrow-right"></i></button>
-		<?php				   
+		<?php
 		}
 	}
 	catch (shopify\ApiException $e)
