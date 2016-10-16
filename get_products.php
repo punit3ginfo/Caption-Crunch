a72251672e86ffbcce471d10b93827179fb1f61f
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-15 19:39:00 -0700

    Update

--- get_products.php
+++ get_products.php
@@ -21,7 +21,7 @@ try
 		if($_REQUEST['colid']=='' && $_REQUEST['status']=='' && !isset($_REQUEST['page_id']) ) {
 
 	     // $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
-		$products = $shopify('GET /admin/products.json', array('limit'=>'21','page'=>'1'));
+		$products = $shopify('GET /admin/products.json', array('limit'=>'27','page'=>'1'));
 			//GET /admin/products.json&limit=50=&page=1
 
 		}
