029c34d4080279fddd5d964a836a89455e7eb853
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-15 17:15:38 -0700

    Update

--- get_products.php
+++ get_products.php
@@ -72,7 +72,7 @@ try
 			}
 
 			?>
-      
+
 <!-- HTML Content for Product  START      -->
 
 <div class="product-card-clearfix">
@@ -127,7 +127,7 @@ try
 
 
                 		//$(_id).html('<button type=button class=share-button onclick=unshareButton('+pid_1+',"'+tags_1+'");>UnShare</button>');
-                		$(_id).html("<button type='button' class='share-button' onclick='unshareButton("+pid_1+","+tags_1+");'>UnShare</button>");
+                		$(_id).html("<button type='button' class='share-button reset-button' onclick='unshareButton("+pid_1+","+tags_1+");'>UnShare</button>");
 
                 		}else{
                 			var _id = '#'+ pid_1;
