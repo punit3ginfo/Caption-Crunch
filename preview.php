2eb67808b724311d0985e67ee7294e6fce4080bf
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-17 20:22:14 -0700

    Update

--- preview.php
+++ preview.php
@@ -1,194 +1,160 @@
 <form class="preview-form-container">
-<!-- CONDITIONS -->
-<div class="preview-form-conatiner">
-  <div class="builder-main-clearfix" style="border-bottom: 0px;">
-      <div class="builder-conditions-container" style="height: 30px;">
+<!--  Step 1  -->
+        <div class="preview-form-conatiner">
+          <div class="builder-main-clearfix" style="border-bottom: 0px;">
+              <div class="builder-conditions-container" style="height: 30px;">
 
-            <span class="conditions-title conditions-title-hover conditions-show-hide-button" style="line-height: 30px; cursor: pointer;">
-              Conditions
-              <i id="condition-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
-            </span>
+                    <span class="conditions-title conditions-title-hover account-show-hide-button" style="line-height: 30px; cursor: pointer;">
+                      Step 1 | Choose Account
+                      <i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
+                    </span>
 
-      </div>
-  </div>
-<div class="conditions-show-hide">
-  <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
-      <div class="builder-conditions-container">
-        <div class="condition-allorany-clearfix">
-            <div class="condition-allorany-container-s">
-              <span>Random Text</span>
-              </div>
-        </div>
-      </div>
-  </div>
-
-  <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
-    <div class="builder-conditions-container" style="text-align: center;">
-          <a href="#" class="add-condition-button"><i class="fa fa-plus" aria-hidden="true"></i> Add another condition</a>
-    </div>
-  </div>
-</div>
-
- </div>
-<!-- TEMPLATE -->
- <div class="preview-form-conatiner" style="height: auto;">
-    <div class="builder-main-clearfix" style="border-bottom: 0px;">
-        <div class="builder-conditions-container" style="height: 30px;">
-            <span class="conditions-title conditions-title-hover templates-show-hide-button" style="line-height: 30px; cursor: pointer;">
-              Template
-              <i id="template-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
-            </span>
-        </div>
-    </div>
-        <div class="template-show-hide">
-          <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
-              <div class="builder-conditions-container">
-                  <span class="" style="line-height: 46px; color: #666;"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right: 10px;"></i> Create a custom caption template or use one of our premade templates</span>
               </div>
           </div>
-          <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
-            <div class="builder-conditions-container" style="height: auto; min-height: 100px;">
-                <textarea style="width: 100%; min-height: 100px; border: 1px solid #666;"></textarea>
-              </div>
-          </div>
-        </div>
-    </div>
-<!-- PRODUCTS -->
- <div class="preview-form-conatiner" style="height: auto;">
-      <div class="builder-main-clearfix" style="border-bottom: 0px;">
-          <div class="builder-conditions-container" style="height: 30px;">
-              <span class="conditions-title conditions-title-hover products-show-hide-button" style="line-height: 30px; cursor: pointer;">
-                  Products
-                  <i id="products-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
-              </span>
-          </div>
+                <div class="account-show-hide">
+                        <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
+                                <div class="builder-conditions-container">
+                                        <div class="condition-allorany-clearfix">
+                                                    <div class="condition-allorany-container-s">
+                                                            <span>Random Text</span>
+                                                    </div>
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
+                                <div class="builder-conditions-container" style="text-align: center;">
+                                      <a href="#Step2" class="add-condition-button">Next Step </a>
+                                </div>
+                        </div>
+                </div>
       </div>
-          <div class="products-show-hide">
-              <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
-                    <div class="builder-conditions-container">
-                        <span class="" style="line-height: 46px; color: #666;"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right: 10px;"></i> Listed below are the products with matching conditions</span>
+      <!--  Step 2  -->
+              <div class="preview-form-conatiner">
+                <div class="builder-main-clearfix" style="border-bottom: 0px;">
+                    <div class="builder-conditions-container" style="height: 30px;">
+
+                          <span class="conditions-title conditions-title-hover caption-show-hide-button" style="line-height: 30px; cursor: pointer;">
+                            Step 2 | Choose Caption
+                            <i id="caption-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
+                          </span>
+
                     </div>
-              </div>
-              <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
-                    <div class="builder-conditions-container" style="height: auto; min-height: 100px; display: block;">
-                      <!-- Product List -->
-                      <div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
-                          <div class="feed-product-main-clearfix">
-                                  <div class="feed-product-image" style="background: #65D35B;">
-                                    <div class="feed-product-image-zoom" style="background: #65D35B;">
+                </div>
+                      <div class="caption-show-hide">
+                              <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
+                                      <div class="builder-conditions-container">
+                                              <div class="condition-allorany-clearfix">
+                                                          <div class="condition-allorany-container-s">
+                                                                  <span>Random Text</span>
+                                                          </div>
+                                              </div>
+                                      </div>
+                              </div>
 
-                                    </div>
-                                  </div>
-                                  <div class="feed-product-titledate-container">
-                                          <span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
-                                  </div>
-                                  <div class="feed-product-sharenow-container">
-                                      <a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                                      <button type='button' class='feed-product-sharenow-button'><i class="fa fa-bullhorn" aria-hidden="true"></i> Share</button>
-                                  </div>
+                              <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
+                                      <div class="builder-conditions-container" style="text-align: center;">
+                                            <a href="#Step3" class="add-condition-button">Next Step </a>
+                                      </div>
+                              </div>
+                      </div>
+            </div>
+            <!--  Step 3  -->
+                    <div class="preview-form-conatiner">
+                      <div class="builder-main-clearfix" style="border-bottom: 0px;">
+                          <div class="builder-conditions-container" style="height: 30px;">
+
+                                <span class="conditions-title conditions-title-hover image-show-hide-button" style="line-height: 30px; cursor: pointer;">
+                                  Step 3 | Add Image
+                                  <i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
+                                </span>
+
                           </div>
                       </div>
-
-                      <div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
-                          <div class="feed-product-main-clearfix">
-                                  <div class="feed-product-image" style="background: #65D35B;">
-                                    <div class="feed-product-image-zoom" style="background: #65D35B;">
-
+                            <div class="image-show-hide">
+                                    <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
+                                            <div class="builder-conditions-container">
+                                                    <div class="condition-allorany-clearfix">
+                                                                <div class="condition-allorany-container-s">
+                                                                        <span>Random Text</span>
+                                                                </div>
+                                                    </div>
+                                            </div>
                                     </div>
-                                  </div>
-                                  <div class="feed-product-titledate-container">
-                                          <span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
-                                  </div>
-                                  <div class="feed-product-sharenow-container">
-                                      <a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                                      <button type='button' class='feed-product-sharenow-button'><i class="fa fa-bullhorn" aria-hidden="true"></i> Share</button>
-                                  </div>
-                          </div>
-                      </div>
 
-                      <div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
-                          <div class="feed-product-main-clearfix">
-                                  <div class="feed-product-image" style="background: #65D35B;">
-                                    <div class="feed-product-image-zoom" style="background: #65D35B;">
-
+                                    <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
+                                            <div class="builder-conditions-container" style="text-align: center;">
+                                                  <a href="#Step4" class="add-condition-button">Next Step </a>
+                                            </div>
                                     </div>
-                                  </div>
-                                  <div class="feed-product-titledate-container">
-                                          <span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
-                                  </div>
-                                  <div class="feed-product-sharenow-container">
-                                      <a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                                      <button type='button' class='feed-product-sharenow-button'><i class="fa fa-bullhorn" aria-hidden="true"></i> Share</button>
-                                  </div>
-                          </div>
-                      </div>
+                            </div>
+                  </div>
+                  <!--  Step 4  -->
+                          <div class="preview-form-conatiner">
+                            <div class="builder-main-clearfix" style="border-bottom: 0px;">
+                                <div class="builder-conditions-container" style="height: 30px;">
 
-                      <div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
-                          <div class="feed-product-main-clearfix">
-                                  <div class="feed-product-image" style="background: #65D35B;">
-                                    <div class="feed-product-image-zoom" style="background: #65D35B;">
+                                      <span class="conditions-title conditions-title-hover share-show-hide-button" style="line-height: 30px; cursor: pointer;">
+                                        Step 4 | Share
+                                        <i id="share-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
+                                      </span>
 
-                                    </div>
-                                  </div>
-                                  <div class="feed-product-titledate-container">
-                                          <span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
-                                  </div>
-                                  <div class="feed-product-sharenow-container">
-                                      <a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                                      <button type='button' class='feed-product-sharenow-button'><i class="fa fa-bullhorn" aria-hidden="true"></i> Share</button>
-                                  </div>
-                          </div>
-                      </div>
+                                </div>
+                            </div>
+                                  <div class="share-show-hide">
+                                          <div class="builder-main-clearfix" style="border-top: 1px solid #eee;">
+                                                  <div class="builder-conditions-container">
+                                                          <div class="condition-allorany-clearfix">
+                                                                      <div class="condition-allorany-container-s">
+                                                                              <span>Random Text</span>
+                                                                      </div>
+                                                          </div>
+                                                  </div>
+                                          </div>
 
-                      <div class="feed-product-main-conatiner" style="width: calc(100% - 60px);">
-                          <div class="feed-product-main-clearfix">
-                                  <div class="feed-product-image" style="background: #65D35B;">
-                                    <div class="feed-product-image-zoom" style="background: #65D35B;">
-
-                                    </div>
+                                          <div class="builder-main-clearfix" style="border-bottom: 0px; padding-bottom: 5px;">
+                                                  <div class="builder-conditions-container" style="text-align: center;">
+                                                        <a href="#" class="add-condition-button">Share Now</a>
+                                                  </div>
+                                          </div>
                                   </div>
-                                  <div class="feed-product-titledate-container">
-                                          <span class="feed-product-title" style="line-height: 46px;">Product Title Goes Here</span>
-                                  </div>
-                                  <div class="feed-product-sharenow-container">
-                                      <a id="preview-button" class='feed-product-preview-button'><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                                      <button type='button' class='feed-product-sharenow-button'><i class="fa fa-bullhorn" aria-hidden="true"></i> Share</button>
-                                  </div>
-                          </div>
-                      </div>
-
-                      <!-- Product List End -->
-                    </div>
-              </div>
-          </div>
-      </div>
+                        </div>
 </form>
 
 <!-- Show / Hide Script -->
 <script type="text/javascript">
 
-	// Condition - Show / Hide
-	$('.conditions-show-hide-button').click(function() {
+	// Step 1 | Choose Account - Show / Hide
+	$('.account-show-hide-button').click(function() {
 		// Show / Hide Conditions Container
-	    $('.conditions-show-hide').slideToggle("slow");
+	    $('.account-show-hide').slideToggle("slow");
 		// Rotate Arrow 180 Degree / Click
-			$('#condition-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
+			$('#account-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
 	});
 
-	// Templates - Show / Hide
-	$('.templates-show-hide-button').click(function() {
-		// Show / Hide Conditions Container
-	    $('.template-show-hide').slideToggle("slow");
-		// Rotate Arrow 180 Degree / Click
-			$('#template-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
-	});
+  // Step 2 | Choose Caption - Show / Hide
+  $('.caption-show-hide-button').click(function() {
+    // Show / Hide Conditions Container
+      $('.caption-show-hide').slideToggle("slow");
+    // Rotate Arrow 180 Degree / Click
+      $('#caption-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
+  });
 
-	// Products - Show / Hide
-	$('.products-show-hide-button').click(function() {
-		// Show / Hide Conditions Container
-	    $('.products-show-hide').slideToggle("slow");
-		// Rotate Arrow 180 Degree / Click
-			$('#products-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
-	});
+  // Step 3 | Add Image - Show / Hide
+  $('.image-show-hide-button').click(function() {
+    // Show / Hide Conditions Container
+      $('.image-show-hide').slideToggle("slow");
+    // Rotate Arrow 180 Degree / Click
+      $('#image-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
+  });
+
+  // Step 4 | Share - Show / Hide
+  $('.share-show-hide-button').click(function() {
+    // Show / Hide Conditions Container
+      $('.share-show-hide').slideToggle("slow");
+    // Rotate Arrow 180 Degree / Click
+      $('#share-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
+  });
+
 
 </script>
