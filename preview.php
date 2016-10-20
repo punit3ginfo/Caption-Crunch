6d6e17f438e6e7697a158de9e822ef6468d53b23
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-20 13:31:30 -0700

    Update

--- preview.php
+++ preview.php
@@ -246,19 +246,23 @@
       $('#share-arrow-icon').toggleClass('fa-chevron-up fa-chevron-down');
   });
 
-  $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+</script>
+
+<script>
+
+$(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('.preview-clearfix').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
     }
-  });
+  }
+});
 });
 
 </script>
