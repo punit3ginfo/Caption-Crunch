429036ce3e87be7eb707cc40b9da85862696a239
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-21 12:36:21 -0700

    Update

--- index.php
+++ index.php
@@ -52,23 +52,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 	<div class="back">
 
     <div id="preview-container">
-              <div class="preview-container">
-              <div  class="preview-overflow-container">
-              <div class="preview-clearfix">
-                          <div class="preview-restrictor" style="z-index: 86;">
-                                  <div class="preview-left-clearfix">
-                                    <?php include 'preview.php';?>
-                                  </div>
-                          </div>
-                          <div class="preview-restrictor" style="position: fixed; z-index: 85;">
-                                  <div class="preview-right-clearfix">
-                                          <?php include 'iphone-preview.php';?>
-                                  </div>
-                        </div>
-                        </div>
-               </div>
-               </div>
-         </div>
+              <?php include 'preview.php';?>
+    </div>
 
     <div id="help-menu-container">
 
