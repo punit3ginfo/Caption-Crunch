5c7ba3d801666eed31c4623cd7cede01f8efa837
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-24 16:45:35 -0700

    Update

--- settings.php
+++ settings.php
@@ -1,3 +1,44 @@
-<?php
-echo "Settings";
-?>
+                      <div class="preview-template-container">
+                          <div class="chat_container" style="overflow: hidden;">
+                                <div class="collection-sidebar-container">
+                                            <div class="collection_title_selector">
+                                                      <div class="collection_title_container">
+                                                          <span class="collection_title_text">Settings</span>
+                                                      </div>
+                                              </div>
+
+                                            <div class="chat_selector_overflow_container">
+                                                  <div class="chat_selector_overflow" style="height: calc(100% - 160px);">
+                                                      <!-- Preview Section -->
+
+
+
+
+                                                       <!-- Preview Section -->
+                                                  </div>
+                                        </div>
+                                 </div>
+
+
+
+                                  <div class="collection_title_header">
+                                                <div class="collection_header_container">
+
+                                                </div>
+                                      </div>
+
+
+
+
+
+                        <div class="product-grid-container scroll-grid">
+                            <div  class="product-grid-overflow-container">
+                              <!-- Form Section -->
+
+
+
+
+                               <!-- Form Section -->
+                            </div>
+                        </div>
+                      </div>
