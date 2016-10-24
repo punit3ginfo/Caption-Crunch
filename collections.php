2420c4445eceefed1a59e3d690b43b7f79ab3d86
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-24 12:39:41 -0700

    Update

--- collections.php
+++ collections.php
@@ -1,32 +1,31 @@
 
-
-<div class="chat_container" style="overflow: hidden;">
-  <div class="collection-sidebar-container">
-              		<div class="collection_title_selector">
-                            <div class="collection_title_container">
-                                <span class="collection_title_text">Collections</span>
-                            </div>
-                    </div>
-
-                	<div class="chat_selector_overflow_container">
-                        <div class="chat_selector_overflow" style="height: calc(100% - 160px);">
-
-                          	<!-- Collection Card -->
-
-
-                                <?php
-
-       				// echo "Product's go here"
-       				require __DIR__.'/get_collectionlist.php'; //GET PRODUCTS
-        				?>
-
-                             <!-- Collection Card -->
-
+<div class="collections-animation-container">
+    <div class="chat_container" style="overflow: hidden;">
+          <div class="collection-sidebar-container">
+                  		<div class="collection_title_selector">
+                                <div class="collection_title_container">
+                                    <span class="collection_title_text">Collections</span>
+                                </div>
                         </div>
-                  	</div>
-              </div>
+
+                    	<div class="chat_selector_overflow_container">
+                            <div class="chat_selector_overflow" style="height: calc(100% - 160px);">
+
+                              	<!-- Collection Card -->
 
 
+                                    <?php
+
+           				// echo "Product's go here"
+           				require __DIR__.'/get_collectionlist.php'; //GET PRODUCTS
+            				?>
+
+                                 <!-- Collection Card -->
+
+                            </div>
+                	</div>
+           </div>
+    </div>
 
 
 						<div class="collection_title_header">
@@ -80,4 +79,8 @@
 
       </div>
   </div>
+</div>
+
+<div id="preview-container">
+          <?php include 'preview.php';?>
 </div>
--- index.php
+++ index.php
@@ -51,10 +51,6 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 <body>
 	<div class="back">
 
-    <div id="preview-container">
-              <?php include 'preview.php';?>
-    </div>
-
     <div id="help-menu-container">
               <?php include 'help.php';?>
     </div>
