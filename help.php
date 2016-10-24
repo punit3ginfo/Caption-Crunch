d1608aee30287c2932a12ea727067ff6aa89e29f
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-23 21:28:06 -0700

    Update

--- help.php
+++ help.php
@@ -1,6 +1,11 @@
 <div class="help-clearfix">
             <div class="help-container">
                       <span class="cc-text-medium">Help Title</span>
-                      <span class="cc-text-small">This is the help widget. <a class="help-link">Help Title</a>is a page anchor to that help section.</span>
+                      <br>
+                      <span class="cc-text-small">There are many variations of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</span>
+                      <br>
+                      <span class="cc-text-small">There are many variations of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</span>
+                      <br>
+                      <span class="cc-text-small">There are many variations of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</span>
             </div>
 </div>
