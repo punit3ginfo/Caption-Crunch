d299d8d9ebdb01fc41bbe1abb48af445f9098276
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-23 21:34:52 -0700

    Update

--- help.php
+++ help.php
@@ -1,7 +1,7 @@
 <div class="help-clearfix">
             <div class="help-container">
                       <span class="cc-text-medium">Help Title</span>
-                      <br>
+                      <br><br>
                       <span class="cc-text-small">There are many of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</span>
                       <br><br>
                       <span class="cc-text-small">There are many variations of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form by randomised words which don't look even slightly believable.</span>
