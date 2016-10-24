73ad3cd0e896b9a5fc3408b49378451962eec09e
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-24 12:23:22 -0700

    Update

--- get_products.php
+++ get_products.php
@@ -213,24 +213,21 @@ $(document).ready(function() {
 
 <!-- Preview Drop Down -->
 <script>
-$(document).ready(function() {
-
-      $('#preview-button-<?php echo $p_id1; ?>').click(function() {
-              $('#preview-container').addClass("preview-container-animate");
-              $('.close-preview-container').css('display', 'flex');
-      });
-
-      $('#close-preview-button').click(function() {
-              $('#preview-container').removeClass("preview-container-animate");
-              $('.close-preview-container').css('display', 'none');
-      });
-
-      $('.sidebar-link').click(function() {
-              $('#preview-container').removeClass("preview-container-animate");
-              $('.close-preview-container').css('display', 'none');
-      });
-
-  });
+// $(document).ready(function() {
+//
+//       $('#preview-button-<?php echo $p_id1; ?>').click(function() {
+//               $('#preview-container').addClass("preview-container-animate");
+//               $('.close-preview-container').css('display', 'flex');
+//       });
+//
+//       $('#close-preview-button').click(function() {
+//               $('#preview-container').removeClass("preview-container-animate");
+//               $('.close-preview-container').css('display', 'none');
+//       });
+//
+//
+//
+//   });
 
   $(document).ready(function() {
 
@@ -242,6 +239,11 @@ $(document).ready(function() {
         $('#close-preview-button').click(function() {
                 $('#preview-container').removeClass("preview-container-animate");
                 $('.close-preview-container').css('display', 'none');
+        });
+
+        $('.sidebar-link').click(function() {
+               $('#preview-container').removeClass("preview-container-animate");
+               $('.close-preview-container').css('display', 'none');
         });
 
     });
