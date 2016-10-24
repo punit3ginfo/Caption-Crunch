db10812dac548b555d2e71b1b8b6e1056641d6be
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-23 21:24:25 -0700

    Update

--- help.php
+++ help.php
@@ -1,5 +1,6 @@
 <div class="help-clearfix">
             <div class="help-container">
-
+                      <span class="cc-text-medium">Help Title</span>
+                      <span class="cc-text-small">This is the help widget. <a class="help-link">Help Title</a>is a page anchor to that help section.</span>
             </div>
 </div>
