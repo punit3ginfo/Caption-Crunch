c686cecfade34c4e8e1e13160640a749a55b50a3
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-24 17:11:56 -0700

    Update

--- iphone-preview.php
+++ iphone-preview.php
@@ -1,4 +1,4 @@
-<div class="marvel-device iphone6 silver">
+<!-- <div class="marvel-device iphone6 silver">
         <div class="top-bar"></div>
         <div class="sleep"></div>
         <div class="volume"></div>
@@ -6,7 +6,7 @@
         <div class="sensor"></div>
         <div class="speaker"></div>
         <div class="screen">
-        <!-- Content goes here -->
+
 
         <div class="facebook-mobile-ios-header">
 
@@ -46,8 +46,64 @@
 
         </div>
 
-        <!-- Content goes here -->
+
         </div>
         <div class="home"></div>
         <div class="bottom-bar"></div>
+</div> -->
+
+
+<div class="wrapper">
+  <div class="iphone">
+    <div class="power"></div>
+    <div class="lock"></div>
+    <div class="volume up"></div>
+    <div class="volume down"></div>
+    <div class="camera"></div>
+    <div class="speaker"></div>
+    <div class="screen">
+      <!-- Put screen stuff in here -->
+      <div class="facebook-mobile-ios-header">
+
+      </div>
+
+      <div class="facebook-mobile-main-container">
+                  <div class="facebook-mobile-header">
+
+                  </div>
+
+                  <div class="facebook-mobile-post-clearfix">
+                              <div class="facebook-mobile-post-container">
+                                          <div class="facebook-mobile-post-header-container">
+                                                    <div class="facebook-mobile-profile"></div>
+                                                    <div class="facebook-mobile-page-name-container">
+                                                                <span class="facebook-mobile-page-name">Facebook Page Name</span>
+                                                    </div>
+                                          </div>
+                                          <div class="facebook-mobile-caption-container">
+                                                      <span class="facebook-mobile-caption-text">
+
+                                                      </span>
+                                          </div>
+                                          <img class="facebook-mobile-image">
+
+                                          </img>
+                                          <div class="facebook-mobile-action-bar">
+
+                                          </div>
+                              </div>
+                  </div>
+      </div>
+
+
+
+      <div class="facebook-mobile-nav">
+
+      </div>
+      
+    </div>
+    <div class="button">
+      <div class="square"></div>
+    </div>
+  </div>
 </div>
