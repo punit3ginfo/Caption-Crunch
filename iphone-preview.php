321fdade869ec1e4e85757c04026908ff4b7bedb
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-24 17:44:28 -0700

    Update

--- iphone-preview.php
+++ iphone-preview.php
@@ -60,47 +60,44 @@
     <div class="camera"></div>
     <div class="speaker"></div>
     <div class="screen">
+              <div class="iphone-screen-overflow">
       <!-- Put screen stuff in here -->
-      <div class="facebook-mobile-ios-header">
+                        <div class="facebook-mobile-ios-header">
 
-      </div>
+                        </div>
 
-      <div class="facebook-mobile-main-container">
-                  <div class="facebook-mobile-header">
+                        <div class="facebook-mobile-main-container">
 
+                                    <div class="facebook-mobile-header"></div>
+
+                                    <div class="facebook-mobile-post-clearfix">
+                                                <div class="facebook-mobile-post-container">
+                                                            <div class="facebook-mobile-post-header-container">
+                                                                      <div class="facebook-mobile-profile"></div>
+                                                                      <div class="facebook-mobile-page-name-container">
+                                                                                  <span class="facebook-mobile-page-name">Facebook Page Name</span>
+                                                                      </div>
+                                                            </div>
+                                                            <div class="facebook-mobile-caption-container">
+                                                                        <span class="facebook-mobile-caption-text">
+
+                                                                        </span>
+                                                            </div>
+                                                            <img class="facebook-mobile-image">
+
+                                                            </img>
+                                                            <div class="facebook-mobile-action-bar">
+
+                                                            </div>
+                                                </div>
+                                    </div>
+                        </div>
+
+                        <div class="facebook-mobile-nav"></div>
+
+                      </div>
                   </div>
-
-                  <div class="facebook-mobile-post-clearfix">
-                              <div class="facebook-mobile-post-container">
-                                          <div class="facebook-mobile-post-header-container">
-                                                    <div class="facebook-mobile-profile"></div>
-                                                    <div class="facebook-mobile-page-name-container">
-                                                                <span class="facebook-mobile-page-name">Facebook Page Name</span>
-                                                    </div>
-                                          </div>
-                                          <div class="facebook-mobile-caption-container">
-                                                      <span class="facebook-mobile-caption-text">
-
-                                                      </span>
-                                          </div>
-                                          <img class="facebook-mobile-image">
-
-                                          </img>
-                                          <div class="facebook-mobile-action-bar">
-
-                                          </div>
-                              </div>
-                  </div>
+              <div class="button">
+          <div class="square"></div>
       </div>
-
-
-
-      <div class="facebook-mobile-nav">
-
-      </div>
-
-    </div>
-    <div class="button">
-      <div class="square"></div>
-    </div>
   </div>
