b3f9a96f236fa23eba8aad6fd3b4e8b611fb1bd6
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-26 13:49:25 -0700

    Update

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
   $('#help-button').click(function() {
       if ($("#help-menu-container").hasClass('help-menu-animate')) {
           $('#help-menu-container').removeClass("help-menu-animate");
+          $('.sidebar-nav-container-header').removeClass("sidebar-nav-container-header-active");
+          $('help-header-link').removeClass("help-header-link-active");
       } else {
           $('#help-menu-container').addClass("help-menu-animate");
+          $('.sidebar-nav-container-header').addClass("sidebar-nav-container-header-active");
+          $('.help-header-link').addClass("help-header-link-active");
       };
   });
 });
