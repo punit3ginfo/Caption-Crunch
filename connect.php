5fb855f9902bd7bfc1dbe290d4732949fcf6e03e
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-27 16:52:54 -0700

    Update

--- connect.php
+++ connect.php
@@ -4,10 +4,12 @@
             <div class="product-card-container">
 
                       <div class="connect-image-container">
+                            <div>
                                 <div class="connect-image shopify-icon">
 
                                 </div>
                                 <span class="cc-text-medium">SHOPIFY</span>
+                           </div>
                       </div>
 
                       <button type='button' class='connected-button' id='shopify-connect-button'><i class='fa fa-check' aria-hidden='true'></i> Connected</button>
@@ -18,10 +20,12 @@
              <div class="product-card-container">
 
                        <div class="connect-image-container">
+                              <div>
                                    <div class="connect-image buffer-icon">
 
                                    </div>
                                    <span class="cc-text-medium">BUFFER</span>
+                              </div>
                        </div>
 
                        <button type='button' class='connect-button' id='shopify-connect-button'><i class='fa fa-sign-in' aria-hidden='true'></i> Connect</button>
