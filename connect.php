98fc3accfd95ca618e6a161b8e0a0f99b40b3424
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-27 10:55:39 -0700

    Update

--- connect.php
+++ connect.php
@@ -5,81 +5,16 @@
           <div class="builder-main-clearfix" style="border-bottom: 1px solid #eee;">
               <div class="builder-conditions-container" style="height: 30px;">
 
-                    <span class="preview-steps-text account-show-hide-button" style="line-height: 30px; cursor: pointer;">
-                      Step 1 | Choose Account
-                      <i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 30px;"></i>
+                    <span class="preview-steps-text" style="line-height: 30px; cursor: pointer;">
+                      Connect Your Accounts
                     </span>
 
               </div>
           </div>
                 <div class="account-show-hide">
                         <div class="builder-main-clearfix" style="">
-                                <div class="builder-conditions-container" style="height: auto; width: 100%; display: inline-block;">
-                                                      <!-- Account Bubbles -->
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
+                                <div class="builder-conditions-container" style="height: auto; width: 100%; display: inline-block; border: 1px solid red;">
 
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-twitter account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-instagram account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-                                                      <!-- Account Bubbles -->
                                 </div>
                         </div>
                 </div>
