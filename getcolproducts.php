b8a25316d1549b2ce43ca8e89a99c3e2111a1187
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-28 11:36:23 -0700

    Update

--- getcolproducts.php
+++ getcolproducts.php
@@ -21,7 +21,7 @@
 		//print_r($products);
 		 $products = $shopify('GET  /admin/products.json?collection_id='.$_REQUEST['colid'], array('published_status'=>'published'));
 		print_r($products);
-		exit; 
+		exit;
 		foreach($products as $singleproduct)
 		{
 			$title=$singleproduct['title']; // Product Title
@@ -39,139 +39,116 @@
 
 			?>
 
-<!-- HTML Content for Product  START      -->
+      <!-- HTML Content for Product  START      -->
 
-<div class="product-card-clearfix">
+      <div class="product-card-clearfix">
 
-	<div class="product-card-container">
+      	<div class="product-card-container">
 
-          <div class="ribbon ribbon-<?php echo $p_id1; ?>"><span>SHARED</span></div>
+                <div class="ribbon ribbon-<?php echo $p_id1; ?>"><span>SHARED</span></div>
 
-      		<div class="product-card-image-container product-image-<?php echo $p_id1; ?>" style='background-image: url(<?php echo $src; ?>)'>
-            <!-- Opacity Layer -->
-              <div class="product-card-image-container-background-hover product-opacity-<?php echo $p_id1; ?>"></div>
-            <!-- Product Details Layer -->
-              <div class="product-card-image-container-content-hover product-details-<?php echo $p_id1; ?>">
-                  <div class="product-details-container">
-                      <div class="product-icon-container" style="margin-bottom: 15px;">
-                         <span class="product-icon-clearfix">
+            		<div class="product-card-image-container product-image-<?php echo $p_id1; ?>" style='background-image: url(<?php echo $src; ?>)'>
+                  <!-- Opacity Layer -->
+                    <div class="product-card-image-container-background-hover product-opacity-<?php echo $p_id1; ?>"></div>
+                  <!-- Product Details Layer -->
+                    <div class="product-card-image-container-content-hover product-details-<?php echo $p_id1; ?>">
+                        <div class="product-details-container">
+                            <div class="product-icon-container" style="margin-bottom: 15px;">
+                               <span class="product-icon-clearfix">
 
-                         </span>
-                      </div>
+                               </span>
+                            </div>
 
-                      <div style="margin-top: 15px;">
-                        <span class="product-title-text"><?php echo $title; ?></span>
-                      </div>
+                            <div style="margin-top: 15px;">
+                              <span class="product-title-text"><?php echo $title; ?></span>
+                            </div>
 
-                      <div style="margin-top: 15px;">
-                        <span class="product-card-price-text" style="margin-right: 3px;">$<?php echo $price; ?></span>
-                        <span class="product-card-price-text" style="font-size: 12px; color: #888;">$<?php echo $price; ?></span>
-                      </div>
-                  </div>
-                  <div class="preview-button-container">
-                    <a id="preview-button-<?php echo $p_id1; ?>" class="preview-button" style="height: 19px; width: calc(100% - 42px);"><i class="fa fa-eye" aria-hidden="true"></i> Preview</a>
-                  </div>
-              </div>
-          </div>
+                            <div style="margin-top: 15px;">
+                              <span class="product-card-price-text" style="margin-right: 3px;">$<?php echo $price; ?></span>
+                              <span class="product-card-price-text" style="font-size: 12px; color: #888;">$<?php echo $price; ?></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-          <div id="<?php echo $p_id1; ?>"  class="share-button-container?>" >
+                <button type='button' class='share-button' id='share-button-<?php echo $p_id1; ?>'><i class='fa fa-bullhorn' aria-hidden='true'></i> Share</button>
 
                 <script>
-                	$(document).ready(function(){
-                		var OrigonalTag = '<?php echo $OrigonalTag; ?>';
 
-                		var pid_1 = '<?php echo $p_id1; ?>';
-                		//alert(pid_1);
-                		var pattern = /shared/;
-                		var pattern1 = / shared/;
+                // Preview
+                $("#share-button-<?php echo $p_id1; ?>").click(function(){
 
-                		var exists = pattern.test(OrigonalTag);
-                		var exists1 = pattern1.test(OrigonalTag);
-                		if(exists || exists1 ){
-                			var tags_1 = '"<?php echo $tags; ?>"';
-                			//alert(tags_1);
+                    var CaptionLong = "<?php echo $title; ?> | $<?php echo $price; ?> | Long Caption";
+                    var CaptionShort = "<?php echo $title; ?> | $<?php echo $price; ?> | Short Caption";
 
-                		        var _id = '#'+ pid_1;
+                    $("#caption-textarea-large").ready(function() {
+                          $("#caption-textarea-large").html(CaptionLong);
+                          $("#caption-textarea-small").html(CaptionShort);
+                    });
 
+                    $(".facebook-mobile-caption-text").ready(function() {
+                          $(".facebook-mobile-caption-text").html(CaptionLong);
+                    });
 
-                		//$(_id).html('<button type=button class=share-button onclick=unshareButton('+pid_1+',"'+tags_1+'");>UnShare</button>');
-                		$(_id).html("<button type='button' class='reset-button'  id='reset-button-<?php echo $p_id1; ?>' onclick='unshareButton("+pid_1+","+tags_1+");'><i class='fa fa-times' aria-hidden='true'></i> Reset</button>");
+                    $("#addimage-image-one").ready(function() {
+                          $("#addimage-image-one").css('background-image', 'url(<?php echo $src; ?>)')
+                    });
 
-                		}else{
-                			var _id = '#'+ pid_1;
-                			<?php
-                			if($OrigonalTag == '')
-                			{
-                			  $OrigonalTag="shared";
-                			}
-                			else{
-                			   $OrigonalTag=$OrigonalTag.",shared";
-                			}
-                			?>
-                			var tags_1 = '"<?php echo $OrigonalTag; ?>"';
+                    var $facebookImageSource = "<?php echo $src; ?>";
 
-                			  //$(_id).html('<button type="button" class=share-button onclick=shareButton('+pid_1+',"'+tags_1+'");>Share</button>');
-                	$(_id).html("<button type='button' class='share-button' id='share-button-<?php echo $p_id1; ?>' onclick='shareButton("+pid_1+","+tags_1+");'><i class='fa fa-bullhorn' aria-hidden='true'></i> Share</button>");
-                		}
-                	});
+                    $(".facebook-mobile-image").ready(function() {
+                          $(".facebook-mobile-image").attr("src", $facebookImageSource);
+                    });
+
+                });
+
+                $('#close-preview-button').click(function() {
+                        // $("#preview-container").empty();
+                        $("#caption-textarea-large").empty();
+                        $("#caption-textarea-small").empty();
+                        $("#addimage-image-one").empty();
+                        $(".facebook-mobile-caption-text").empty();
+                        $(".facebook-mobile-image").attr("src", "");
+                });
+
+                // Show / Hide Product Details
+                $(document).ready(function() {
+                      $('.product-image-<?php echo $p_id1; ?>').hover(function() {
+                        // Show / Hide Product Details Opacity Container
+                          $('.product-opacity-<?php echo $p_id1; ?>').toggle();
+                        // Show / Hide Product Details Container
+                          $('.product-details-<?php echo $p_id1; ?>').toggle();
+                      });
+                  });
+
+                  // Preview Drop Down
+                  $(document).ready(function() {
+
+                        $('.sidebar-link').click(function() {
+                               $('#preview-container').removeClass("preview-container-animate");
+                               $('.close-preview-container').css('display', 'none');
+                        });
+
+                        $('#share-button-<?php echo $p_id1; ?>').click(function() {
+                                $('.collections-animation-container').addClass("collections-animation");
+                                $('#preview-container').addClass("preview-container-animate");
+                                $('.close-preview-container').css('display', 'flex');
+                        });
+
+                        $('#close-preview-button').click(function() {
+                                $('.collections-animation-container').removeClass("collections-animation");
+                                $('#preview-container').removeClass("preview-container-animate");
+                                $('.close-preview-container').css('display', 'none');
+                        });
+
+                    });
+
                 </script>
 
-	           </div>
+              </div>
 
-      </div>
-
-</div>
-
-<!-- Show / Hide Product Details -->
-<script>
-  $('.product-image-<?php echo $p_id1; ?>').hover(function() {
-    // Show / Hide Product Details Opacity Container
-      $('.product-opacity-<?php echo $p_id1; ?>').toggle();
-    // Show / Hide Product Details Container
-      $('.product-details-<?php echo $p_id1; ?>').toggle();
-  });
-</script>
-
-<!-- Preview Drop Down -->
-<script>
-$(document).ready(function(){
-
-      $('#preview-button-<?php echo $p_id1; ?>').click(function() {
-              $('#preview-container').addClass("preview-container-animate");
-              $('.close-preview-container').css('display', 'flex');
-      });
-
-      $('#close-preview-button').click(function() {
-              $('#preview-container').removeClass("preview-container-animate");
-              $('.close-preview-container').css('display', 'none');
-      });
-
-  });
-</script>
-
-<!-- Shared Banner -->
-<script>
-$(document).ready(function(){
-    if ( $( "#reset-button-<?php echo $p_id1; ?>" ).length != 0) {
-      $('.ribbon-<?php echo $p_id1; ?>').css( "display", "block" );
-    } else {
-      $('.ribbon-<?php echo $p_id1; ?>').css( "display", "none" );
-    }
-});
-
-$(document).ready(function(){
-    $( "#reset-button-<?php echo $p_id1; ?>" ).click( function()  {
-      $('.ribbon-<?php echo $p_id1; ?>').css( "display", "none" );
-    });
-});
-
-$(document).ready(function(){
-    $( "#share-button-<?php echo $p_id1; ?>" ).click( function()  {
-      $('.ribbon-<?php echo $p_id1; ?>').css( "display", "block" );
-    });
-});
-</script>
-<!-- HTML Content for Product END    -->
+          </div>
+      <!-- HTML Content for Product END    -->
 
 	<?php
 
