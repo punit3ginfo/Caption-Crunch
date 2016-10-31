05e6e1ecc536bd273882bf797f5d1c32332a9934
Lucas Sailer <LucasJSailer@gmail.com>
2016-10-31 16:58:43 -0700

    Update

--- collections.php
+++ collections.php
@@ -42,24 +42,25 @@
 
 
 
-						<div class="collection_title_header">
+						<div class="content-header">
 
-                          <div class="collection-share-history-container">
+                          <div class="content-header-3col-container">
                             <a href="javascript:void(0)" onclick="getsharehistory()" class="sidebar-span share-history-button">
 				                        <i class="fa fa-history" aria-hidden="true"></i>
 				                         History
 			                      </a>
                           </div>
 
-                          <div class="collection_header_container">
+                          <div class="content-header-3col-container">
+                                      <input class="search-text-box" onKeyDown="if(event.keyCode==13) search(document.getElementById('formValueId').value);" id="formValueId" type="text" name="q_name" placeholder="Search" />
+                                      <i style="position: absolute; right: 10px;  color: #666;" class="fa fa-search" onclick="search(document.getElementById('formValueId').value);"></i>
+                          </div>
 
-                            <div class="pagination-clearfix">
-                                          <div class="text-center" id="pagination">
-
-                                          </div>
-                          	</div>
-
-                            <div class="collection_title_header_container">
+                          <div class="content-header-3col-container">
+                                      <div class="text-center" id="pagination"></div>
+                          </div>
+                          
+                            <!-- <div class="collection_title_header_container">
                                <div class="collection_title_container-active">
 
                                     <input class="search-text-box" onKeyDown="if(event.keyCode==13) search(document.getElementById('formValueId').value);" id="formValueId" type="text" name="q_name" placeholder="Search" />
@@ -67,9 +68,7 @@
                                     <i style="position: absolute; right: 10px;  color: #666;" class="fa fa-search" onclick="search(document.getElementById('formValueId').value);"></i>
 
                                </div>
-                            </div>
-
-                          </div>
+                            </div> -->
         				</div>
 
 
