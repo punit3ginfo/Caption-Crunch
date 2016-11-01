c800287beeb976815ffaac95bc0bc6c737beb72d
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-01 15:28:46 -0700

    Update

--- preview.php
+++ preview.php
@@ -1,48 +1,46 @@
 <div class="preview-container">
           <div  class="preview-overflow-container">
                     <div class="preview-clearfix">
-                      <div class="preview-template-container">
-                          <div class="chat_container" style="overflow: hidden;">
-                                <div class="collection-sidebar-container">
-                                            <div class="collection_title_selector">
-                                                      <div class="collection_title_container">
-                                                          <span class="collection_title_text">Preview</span>
+                              <div class="preview-template-container">
+                                      <div class="chat_container" style="overflow: hidden;">
+
+                                                <div class="collection-sidebar-container">
+                                                                <div class="collection_title_selector">
+                                                                          <div class="collection_title_container">
+                                                                              <span class="cc-text-medium">Preview</span>
+                                                                          </div>
+                                                                  </div>
+
+                                                                 <div class="menu-overflow-container">
+                                                                            <div class="menu-overflow" style="overflow: hidden; display: flex; justify-content: center; align-items: center;">
+                                                                            <!-- Preview Section -->
+
+                                                                              <?php include 'iphone-preview.php';?>
+
+                                                                             <!-- Preview Section -->
+                                                                              </div>
+                                                                   </div>
+                                                     </div>
+
+
+
+                                                      <div class="content-header preview-header">
+                                                                <a href="#" class="button-four-col preview-share-button"><i class="fa fa-bullhorn" aria-hidden="true"></i> Share Now</a>
+                                                                <a href="#" class="button-four-col preview-sharenext-button"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i> Share Next</a>
+                                                                <a href="#" class="button-four-col preview-sharelater-button"><i class="fa fa-clock-o" aria-hidden="true"></i> Share Later</a>
+                                                                <a href="#" class="button-four-col preview-schedule-button"><i class="fa fa-calendar" aria-hidden="true"></i> Schedule</a>
                                                       </div>
-                                              </div>
 
-                                              <div class="menu-overflow-container">
-                                                    <div class="menu-overflow" style="overflow: hidden; display: flex; justify-content: center; align-items: center;">
-                                                      <!-- Preview Section -->
+                                                      <div class="content-overflow-container">
+                                                                <div  class="content-overflow">
+                                                                  <!-- Form Section -->
+                                                                          <?php include 'preview-form.php';?>
+                                                                   <!-- Form Section -->
+                                                                </div>
+                                                      </div>
 
-                                                        <?php include 'iphone-preview.php';?>
-
-                                                       <!-- Preview Section -->
-                                                  </div>
-                                        </div>
+                                            </div>
                                  </div>
-
-
-
-                                  <div class="content-header preview-header">
-                                          <a href="#" class="button-four-col preview-share-button"><i class="fa fa-bullhorn" aria-hidden="true"></i> Share Now</a>
-                                          <a href="#" class="button-four-col preview-sharenext-button"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i> Share Next</a>
-                                          <a href="#" class="button-four-col preview-sharelater-button"><i class="fa fa-clock-o" aria-hidden="true"></i> Share Later</a>
-                                          <a href="#" class="button-four-col preview-schedule-button"><i class="fa fa-calendar" aria-hidden="true"></i> Schedule</a>
-                                  </div>
-
-
-
-
-
-                                  <div class="content-overflow-container">
-                                      <div  class="content-overflow">
-                                        <!-- Form Section -->
-                                                <?php include 'preview-form.php';?>
-                                         <!-- Form Section -->
-                                      </div>
-                                  </div>
-                            </div>
-                    </div>
+                      </div>
            </div>
- </div>
-  </div>
+</div>
