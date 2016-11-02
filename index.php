f2be11b9162d967a691ed48a00bf37a50b846dcd
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-02 12:05:17 -0700

    Update

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 
             <a class="sidebar-link-active" href="javascript:void(0)" id="back-link" style="display: none;">
             	<div class="sidebar-nav-container">
-                  	<span class="sidebar-span" style="color: #65D35B;">
+                  	<span class="sidebar-span">
                       <i style="font-size: 20px;" class="fa fa-angle-double-left" aria-hidden="true"></i><br>
                       Back
                     </span>
