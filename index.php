06bf07c197cfe1efce79663d54204f9127681b00
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-01 19:36:02 -0700

    Update

--- index.php
+++ index.php
@@ -176,7 +176,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
           $('#share-link').addClass('sidebar-link-active');
           $('#captions-link').removeClass('sidebar-link-active');
           $('#settings-link').removeClass('sidebar-link-active');
-          $(".help-clearfix").load("help/share.php";
+          $(".help-clearfix").load("help/share.php");
                     }
                 });
             }
