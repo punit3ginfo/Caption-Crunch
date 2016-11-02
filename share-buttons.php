e82376848164e2b3dc8e0e4ed3601c794a054835
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-02 12:26:49 -0700

    Update

---
+++ share-buttons.php
@@ -0,0 +1,4 @@
+<a href="javascript:void(0)" class="button-four-col preview-share-button"><i class="fa fa-bullhorn" aria-hidden="true"></i> Share Now</a>
+<a href="javascript:void(0)" class="button-four-col preview-sharenext-button"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i> Share Next</a>
+<a href="javascript:void(0)" class="button-four-col preview-sharelater-button"><i class="fa fa-clock-o" aria-hidden="true"></i> Share Later</a>
+<a href="javascript:void(0)" class="button-four-col preview-schedule-button"><i class="fa fa-calendar" aria-hidden="true"></i> Schedule</a>
