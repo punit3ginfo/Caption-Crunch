fd98654eb2ba9af436ee55dfa4e201140af8aada
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-03 20:03:00 -0700

    Update

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,1 @@
-<iframe style="width: 100%; height: 100%;" src="https://buffer.com/app/profile/57acc3a6a2acf818412334fa/buffer/queue/list"></iframe>
+<iframe style="width: 100%; height: 100%;" src="https://buffer.com"></iframe>
