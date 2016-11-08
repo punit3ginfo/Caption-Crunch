0f2c804f9ce287e9985442c839bc57627e127c62
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-08 09:53:45 -0800

    Update

--- product-preview.php
+++ product-preview.php
@@ -2,7 +2,7 @@
           <div  class="preview-overflow-container">
                     <div class="preview-clearfix">
                               <div class="preview-template-container">
-                                          <iframe id="product-preview" src="https://www.google.ca" style="width: 100%; height: 100%;"></iframe>
+                                          <iframe id="product-preview" src="//www.google.ca" style="width: 100%; height: 100%;"></iframe>
                                  </div>
                       </div>
            </div>
