e4296c2fdd04dd6188b7f3984587012e656624c7
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-14 21:15:23 -0800

    u

--- iphone-preview.php
+++ iphone-preview.php
@@ -1,42 +1,42 @@
-  <div class="iphone">
-    <div class="power"></div>
-    <div class="lock"></div>
-    <div class="volume up"></div>
-    <div class="volume down"></div>
-    <div class="camera"></div>
-    <div class="speaker"></div>
-    <div class="screen">
-              <div class="facebook-mobile-ios-header"></div>
+<div class="iphone">
+	<div class="power"></div>
+	<div class="lock"></div>
+	<div class="volume up"></div>
+	<div class="volume down"></div>
+	<div class="camera"></div>
+	<div class="speaker"></div>
+	<div class="screen">
+		<div class="facebook-mobile-ios-header"></div>
 
-              <div class="iphone-screen-overflow">
-                    <!-- Put screen stuff in here -->
-                        <div class="facebook-mobile-main-container">
+		<div class="iphone-screen-overflow">
+			<!-- Put screen stuff in here -->
+			<div class="facebook-mobile-main-container">
 
-                                    <div class="facebook-mobile-header"></div>
+				<div class="facebook-mobile-header"></div>
 
-                                    <div class="facebook-mobile-post-clearfix">
-                                                <div class="facebook-mobile-post-container">
-                                                            <div class="facebook-mobile-post-header-container">
-                                                                      <div class="facebook-mobile-profile"></div>
-                                                                      <div class="facebook-mobile-page-name-container">
-                                                                                  <span class="facebook-mobile-page-name">Facebook Page Name</span>
-                                                                      </div>
-                                                            </div>
-                                                            <div class="facebook-mobile-caption-container">
-                                                                        <span class="facebook-mobile-caption-text">
+				<div class="facebook-mobile-post-clearfix">
+					<div class="facebook-mobile-post-container">
+						<div class="facebook-mobile-post-header-container">
+							<div class="facebook-mobile-profile"></div>
+							<div class="facebook-mobile-page-name-container">
+								<span class="facebook-mobile-page-name">Facebook Page Name</span>
+							</div>
+						</div>
+						<div class="facebook-mobile-caption-container">
+							<span class="facebook-mobile-caption-text">
 
-                                                                        </span>
-                                                            </div>
-                                                            <img class="facebook-mobile-image"></img>
+							</span>
+						</div>
+						<img class="facebook-mobile-image"></img>
 
-                                                            <div class="facebook-mobile-action-bar"></div>
-                                                </div>
-                                    </div>
-                              </div>
-                      </div>
+						<div class="facebook-mobile-action-bar"></div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                      <div class="facebook-mobile-nav"></div>
+		<div class="facebook-mobile-nav"></div>
 
-                  </div>
-              <div class="button"></div>
-  </div>
+	</div>
+	<div class="button"></div>
+</div>
--- preview-form.php
+++ preview-form.php
@@ -1,122 +1,122 @@
 <form class="form">
-<!--  Step 1  -->
-        <div class="form-container" style="padding-bottom: 0px">
+	<!--  Step 1  -->
+	<div class="form-container" style="padding-bottom: 0px">
 
 
-              <div class="form-clearfix" style="border-top: 0px;">
+		<div class="form-clearfix" style="border-top: 0px;">
 
-                    <span class="cc-text-medium form-text-header account-show-hide-button">
-                      Step 1 | Choose Profile
-                      <i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-                    </span>
+			<span class="cc-text-medium form-text-header account-show-hide-button">
+				Step 1 | Choose Profile
+				<i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+			</span>
 
-              </div>
+		</div>
 
-                <div class="account-show-hide">
-                        <div class="form-clearfix">
+		<div class="account-show-hide">
+			<div class="form-clearfix">
 
-                                                      <!-- Account Bubbles -->
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
+				<!-- Account Bubbles -->
+				<div class="account-bubble-clearfix">
+					<a href="/#">
+						<div class="account-bubble">
+							<i class="fa fa-facebook account-icon" aria-hidden="true"></i>
+						</div>
+					</a>
+				</div>
 
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-twitter account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
+				<div class="account-bubble-clearfix">
+					<a href="/#">
+						<div class="account-bubble">
+							<i class="fa fa-twitter account-icon" aria-hidden="true"></i>
+						</div>
+					</a>
+				</div>
 
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
+				<div class="account-bubble-clearfix">
+					<a href="/#">
+						<div class="account-bubble">
+							<i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
+						</div>
+					</a>
+				</div>
 
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
+				<div class="account-bubble-clearfix">
+					<a href="/#">
+						<div class="account-bubble">
+							<i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
+						</div>
+					</a>
+				</div>
 
-                                                            <div class="account-bubble-clearfix">
-                                                                    <a href="/#">
-                                                                              <div class="account-bubble">
-                                                                                        <i class="fa fa-instagram account-icon" aria-hidden="true"></i>
-                                                                              </div>
-                                                                    </a>
-                                                            </div>
-                                                      <!-- Account Bubbles -->
+				<div class="account-bubble-clearfix">
+					<a href="/#">
+						<div class="account-bubble">
+							<i class="fa fa-instagram account-icon" aria-hidden="true"></i>
+						</div>
+					</a>
+				</div>
+				<!-- Account Bubbles -->
 
-                        </div>
-                </div>
-      </div>
-      <!--  Step 2  -->
-      <div class="form-container" style="padding-bottom: 0px">
-                <div id="step2" class="preview-anchor"></div>
-                <div class="form-clearfix"  style="border-top: 0px">
+			</div>
+		</div>
+	</div>
+	<!--  Step 2  -->
+	<div class="form-container" style="padding-bottom: 0px">
+		<div id="step2" class="preview-anchor"></div>
+		<div class="form-clearfix"  style="border-top: 0px">
 
-                          <span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-                            Step 2 | Choose Caption
-                            <i id="caption-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-                          </span>
+			<span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
+				Step 2 | Choose Caption
+				<i id="caption-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+			</span>
 
-                </div>
-                      <div class="caption-show-hide">
-                              <div class="form-clearfix">
-                                      <div class="form-caption-list-clearfix">
-                                          <a href="javascript:void(0)" class="button-three-col green-button" id="caption-one"><i class="fa fa-check" aria-hidden="true"></i> Caption One</a>
-                                          <a href="javascript:void(0)" class="button-three-col grey-button" id="caption-two"><i class="fa fa-times" aria-hidden="true"></i> Caption Two</a>
-                                          <a href="javascript:void(0)" class="button-three-col grey-button" id="caption-three"><i class="fa fa-times" aria-hidden="true"></i> Caption Three</a>
-                                      </div>
-                              </div>
-                              <div class="form-clearfix">
-                                                <span class="cc-text-small">Caption:</span>
-                                                <div class="caption-textarea-container" style="padding-bottom: 0px;">
-                                                      <textarea id="preview-textarea-large" class="caption-textarea"></textarea>
-                                                </div>
-                                                <!-- <span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
-                                                <div class="caption-textarea-container" style="margin-bottom: 0px;">
-                                                      <textarea id="preview-textarea-small" class="caption-textarea"></textarea>
-                                                </div> -->
-                              </div>
-                      </div>
+		</div>
+		<div class="caption-show-hide">
+			<div class="form-clearfix">
+				<div class="form-caption-list-clearfix">
+					<a href="javascript:void(0)" class="button-three-col green-button" id="caption-one"><i class="fa fa-check" aria-hidden="true"></i> Caption One</a>
+					<a href="javascript:void(0)" class="button-three-col grey-button" id="caption-two"><i class="fa fa-times" aria-hidden="true"></i> Caption Two</a>
+					<a href="javascript:void(0)" class="button-three-col grey-button" id="caption-three"><i class="fa fa-times" aria-hidden="true"></i> Caption Three</a>
+				</div>
+			</div>
+			<div class="form-clearfix">
+				<span class="cc-text-small">Caption:</span>
+				<div class="caption-textarea-container" style="padding-bottom: 0px;">
+					<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
+				</div>
+				<!-- <span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
+				<div class="caption-textarea-container" style="margin-bottom: 0px;">
+				<textarea id="preview-textarea-small" class="caption-textarea"></textarea>
+			</div> -->
+		</div>
+	</div>
 </div>
 <div class="form-container" style="padding-bottom: 0px">
-            <!--  Step 3  -->
+	<!--  Step 3  -->
 
-                      <div id="step3" class="preview-anchor-2"></div>
-                      <div class="form-clearfix"  style="border-top: 0px">
-                                <span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-                                  Step 3 | Add Image
-                                  <i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-                                </span>
-                      </div>
-                            <div class="image-show-hide">
-                                    <div class="form-clearfix">
-                                                <div class="addimage-clearfix">
-                                                        <div id="addimage-image-one" class="addimage-container">
+	<div id="step3" class="preview-anchor-2"></div>
+	<div class="form-clearfix"  style="border-top: 0px">
+		<span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
+			Step 3 | Add Image
+			<i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+		</span>
+	</div>
+	<div class="image-show-hide">
+		<div class="form-clearfix">
+			<div class="addimage-clearfix">
+				<div id="addimage-image-one" class="addimage-container">
 
-                                                        </div>
-                                                </div>
+				</div>
+			</div>
 
-                                                <!-- <div class="addimage-clearfix">
-                                                                                  <div class="addimage-container addimage-active">
-                                                                                              <i class="fa fa-plus addimage-plus-icon" aria-hidden="true"></i>
-                                                                                  </div>
-                                                                          </div> -->
-                                    </div>
-                            </div>
-                  </div>
+			<!-- <div class="addimage-clearfix">
+			<div class="addimage-container addimage-active">
+			<i class="fa fa-plus addimage-plus-icon" aria-hidden="true"></i>
+		</div>
+	</div> -->
+</div>
+</div>
+</div>
 </form>
 
 
