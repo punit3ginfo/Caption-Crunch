6c8eade6477f8ea806bb2b3655679253f8194d16
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-15 16:16:19 -0800

    u

--- iphone-preview.php
+++ iphone-preview.php
@@ -1,43 +1,44 @@
-<div class="iphone">
-	<div class="power"></div>
-	<div class="lock"></div>
-	<div class="volume up"></div>
-	<div class="volume down"></div>
-	<div class="camera"></div>
-	<div class="speaker"></div>
-	<div class="screen">
-		<div class="facebook-mobile-ios-header"></div>
-
-		<div class="iphone-screen-overflow">
-			<!-- Put screen stuff in here -->
-			<div class="facebook-mobile-main-container">
-
-				<div class="facebook-mobile-header"></div>
-
-				<div class="facebook-mobile-post-clearfix">
-					<div class="facebook-mobile-post-container">
-						<div class="facebook-mobile-post-header-container">
-							<div class="facebook-mobile-profile"></div>
-							<div class="facebook-mobile-page-name-container">
-								<span class="facebook-mobile-page-name">Facebook Page Name</span>
-								<span class="facebook-mobile-page-time">2 mins</span>
+<div>
+	<div class="iphone">
+		<div class="power"></div>
+		<div class="lock"></div>
+		<div class="volume up"></div>
+		<div class="volume down"></div>
+		<div class="camera"></div>
+		<div class="speaker"></div>
+		<div class="screen">
+			<div class="facebook-mobile-ios-header"></div>
+			<div class="iphone-screen-overflow">
+				<!-- Put screen stuff in here -->
+				<div class="facebook-mobile-main-container">
+					<div class="facebook-mobile-header"></div>
+					<div class="facebook-mobile-post-clearfix">
+						<div class="facebook-mobile-post-container">
+							<div class="facebook-mobile-post-header-container">
+								<div class="facebook-mobile-profile"></div>
+								<div class="facebook-mobile-page-name-container">
+									<span class="facebook-mobile-page-name">Facebook Page Name</span>
+									<span class="facebook-mobile-page-time">2 mins</span>
+								</div>
 							</div>
-						</div>
-						<div class="facebook-mobile-caption-container">
-							<span class="facebook-mobile-caption-text">
+							<div class="facebook-mobile-caption-container">
+								<span class="facebook-mobile-caption-text">
 
-							</span>
-						</div>
-						<img class="facebook-mobile-image"></img>
+								</span>
+							</div>
+							<img class="facebook-mobile-image"></img>
 
-						<div class="facebook-mobile-action-bar"></div>
+							<div class="facebook-mobile-action-bar"></div>
+						</div>
 					</div>
 				</div>
 			</div>
+			<div class="facebook-mobile-nav"></div>
 		</div>
-
-		<div class="facebook-mobile-nav"></div>
+		<div class="button"></div>
+	</div>
+	
+	<div class="preview-buttons-container">
 
 	</div>
-	<div class="button"></div>
 </div>
