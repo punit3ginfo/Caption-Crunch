b9223870e7377e46f44419a6526cb54c611a1a3a
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-16 15:00:35 -0800

    u

--- caption-form.php
+++ caption-form.php
@@ -14,31 +14,31 @@
 	<div class="template-show-hide">
 		<div class="form-clearfix">
 			<div class="form-caption-list-clearfix">
-				<a id="product-title-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-title-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-title-icon" class="fa fa-times" aria-hidden="true"></i> Product Title
 				</a>
-				<a id="product-price-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-price-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-price-icon" class="fa fa-times" aria-hidden="true"></i> Product Price
 				</a>
-				<a id="product-comp-price-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-comp-price-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-comp-price-icon" class="fa fa-times" aria-hidden="true"></i> Compare Price
 				</a>
-				<a id="product-type-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-type-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i  id="product-type-icon" class="fa fa-times" aria-hidden="true"></i> Product Type
 				</a>
-				<a id="product-url-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-url-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-url-icon" class="fa fa-times" aria-hidden="true"></i> Product Url
 				</a>
-				<a id="product-description-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-description-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-description-icon" class="fa fa-times" aria-hidden="true"></i> Description
 				</a>
-				<a id="product-collection-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-collection-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-collection-icon" class="fa fa-times" aria-hidden="true"></i> Collection Title
 				</a>
-				<a id="product-percent-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-percent-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-percent-icon" class="fa fa-times" aria-hidden="true"></i> Precent Off
 				</a>
-				<a id="product-dollars-btn" href="javascript:void(0)" class="button-three-col grey-button">
+				<a id="product-dollars-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
 					<i id="product-dollars-icon" class="fa fa-times" aria-hidden="true"></i> Dollars Saved
 				</a>
 			</div>
