94f3bf3bea9a446708a2aed19e3c3eccc3890f49
punit <punit.3ginfo@gmail.com>
2016-11-18 13:32:51 +0530

    Update collections.php

--- collections.php
+++ collections.php
@@ -79,7 +79,7 @@
 					  <nav aria-label="Page navigation">
        						 <ul class="pagination" id="pagination1"></ul>
    					 </nav>
-                                          <div class="text-center" id="pagination"></div>
+                                          <!--div class="text-center" id="pagination"--></div>
                                   </div>
                           </div>
 
