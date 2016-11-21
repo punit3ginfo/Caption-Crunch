d5241728beac6eec655a9da2a1e556a458685c3d
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-20 16:48:37 -0800

    u

--- help/share.php
+++ help/share.php
@@ -1,9 +1,25 @@
 <div class="help-container">
-          <span class="cc-text-medium">Share</span>
+          <span class="cc-text-medium">The Share Page</span>
           <br><br>
-          <span class="cc-text-small">There are many of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</span>
-          <br><br>
-          <span class="cc-text-small">There are many variations of passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form by randomised words which don't look even slightly believable.</span>
-          <br><br>
-          <span class="cc-text-small">There are passages of <a class="help-link">Lorem Ipsum</a> available, but the majority have suffered alteration in some form, by injected humour, or randomised words which look  believable.</span>
+          <span class="cc-text-small">The share page is primarily used for manually sharing and scheduling. On the right is a list of your collections and on the left is a grid showing the products of the indicated collection. In the header above the product grid is three controls called: History, Search, & Pagination.</span>
 </div>
+
+<div class="help-container">
+          <span class="cc-text-medium">Share History</span>
+          <br><br>
+          <span class="cc-text-small">The share page is primarily used for manually sharing and scheduling. On the right is a list of your collections and on the left is a grid showing the products of the indicated collection. In the header above the product grid is three controls called: History, Search, & Pagination.</span>
+</div>
+
+<div class="help-container">
+          <span class="cc-text-medium">Search</span>
+          <br><br>
+          <span class="cc-text-small">The share page is primarily used for manually sharing and scheduling. On the right is a list of your collections and on the left is a grid showing the products of the indicated collection. In the header above the product grid is three controls called: History, Search, & Pagination.</span>
+</div>
+
+<div class="help-container">
+          <span class="cc-text-medium">Pagination</span>
+          <br><br>
+          <span class="cc-text-small">The share page is primarily used for manually sharing and scheduling. On the right is a list of your collections and on the left is a grid showing the products of the indicated collection. In the header above the product grid is three controls called: History, Search, & Pagination.</span>
+</div>
+
+<!-- <a class="help-link">Lorem Ipsum</a>  -->
