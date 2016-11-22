a63aed3bb492c9276e0e984687e1d05b8ba8ebae
punit <punit.3ginfo@gmail.com>
2016-11-22 13:06:49 +0530

    Update preview.php

--- preview.php
+++ preview.php
@@ -56,28 +56,6 @@ try
 		}
 
 		?>
-<div class="preview-container" id="post-preview-container">
-	<div  class="preview-overflow-container">
-		<div class="preview-clearfix">
-			<div class="preview-template-container">
-				<div class="chat_container" style="overflow: hidden;">
-					<div class="sidebar-content-header">
-						<div class="content-header-title-container">
-							<div class="content-header-title">
-								<span class="cc-text-large">
-									Preview
-								</span>
-							</div>
-						</div>
-
-						<div class="menu-overflow-container">
-							<div class="menu-overflow" style="overflow: hidden; display: flex; justify-content: center; align-items: center;">
-								<!-- Preview Section -->
-								<?php include 'iphone-preview.php';?>
-								<!-- Preview Section -->
-							</div>
-						</div>
-					</div>
 
 					<div id="<?php echo $p_id1; ?>">
 						<script>
@@ -157,17 +135,4 @@ try
                      }
                  });
              }
-     </script>
-
-					<div class="content-overflow-container">
-						<div  class="preview-content-overflow">
-							<!-- Form Section -->
-							<?php include 'preview-form.php';?>
-							<!-- Form Section -->
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+     </script>		
