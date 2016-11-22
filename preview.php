04e832f8422e3a898fc58138b9f8275266c46282
punit <punit.3ginfo@gmail.com>
2016-11-22 12:16:39 +0530

    Update preview.php

--- preview.php
+++ preview.php
@@ -1,3 +1,84 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+$access_token=$_REQUEST['access_token'];
+$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
+
+?>
+
+<?php
+try
+{
+	# Making an API request can throw an exception
+	if(isset($_REQUEST['colid']) && $_REQUEST['colid']!='' && $_REQUEST['status']=='' && !(isset($_REQUEST['page_id']))){
+      //echo "<script>alert(1);</script>";
+
+		$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('published_status'=>'published','limit'=>'21','page'=>'1'));
+		//$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('limit'=>'12','page'=>'2'));
+
+	}
+	if($_REQUEST['colid']=='' && $_REQUEST['status']=='' && !isset($_REQUEST['page_id']) ) {
+  // echo "<script>alert(2);</script>";
+		// $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		$products = $shopify('GET /admin/products.json', array('limit'=>'21','page'=>'1'));
+		//GET /admin/products.json&limit=50=&page=1
+
+	}
+	if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) && (isset($_REQUEST['colid']))) {
+             //echo "limit".$_REQUEST['limit'];
+		// $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		$products = $shopify('GET /admin/products.json?collection_id='.$_REQUEST['colid'], array('published_status'=>'published','limit'=>'21','page'=>$_REQUEST['page_id']));
+		//GET /admin/products.json&limit=50=&page=1
+
+	}
+	if($_REQUEST['page_id']!='' && isset($_REQUEST['page_id']) && $_REQUEST['limit']!='' && isset($_REQUEST['limit']) && $_REQUEST['colid']=='' ) {
+           //  echo "<script>alert(4);</script>";
+		// $products = $shopify('GET /admin/products.json', array('published_status'=>'published'));
+		$products = $shopify('GET /admin/products.json', array('limit'=>$_REQUEST['limit'],'page'=>$_REQUEST['page_id']));
+		//GET /admin/products.json&limit=50=&page=1
+
+	}
+
+	$shop = $shopify('GET /admin/shop.json', array('limit'=>$_REQUEST['limit'],'page'=>$_REQUEST['page_id']));
+
+	$count=0;
+
+	// foreach($shop as $shopdetails)
+	// {
+	//     $domain=$shopdetails['domain'];
+	// }
+
+	foreach($products as $singleproduct)
+	{
+		$title=$singleproduct['title']; // Product Title
+		$variants=$singleproduct['variants'];
+		$p_id1=$singleproduct['id'];
+		$tags=$singleproduct['tags'];
+		$OrigonalTag=$singleproduct['tags'];
+		$ComparePrice=$singleproduct['compare_at_price'];
+		$handle=$singleproduct['handle'];
+
+
+		$tags = str_replace('shared', '', $tags);
+		$tags = str_replace(' ', '', $tags);
+		$tags = str_replace(',', 'AA', $tags);
+		//$tags = str_replace(',shared', '', $tags);
+		//$tags = str_replace('shared', '', $tags);
+		//$tags = str_replace(',', 'AA', $tags);
+		foreach($variants as $variants){
+			$price=$variants['price']; // Product PRice
+			$ComparePrice=$variants['compare_at_price'];
+		}
+		$images=$singleproduct['images'];
+
+		foreach($images as $images){
+			$src=$images['src']; //Image Source
+		}
+
+		$productUrl = "http://mens.fashiondeals.today/products/$handle";
+
+		?>
 <div class="preview-container" id="post-preview-container">
 	<div  class="preview-overflow-container">
 		<div class="preview-clearfix">
@@ -37,9 +118,7 @@
           var exists1 = pattern1.test(OrigonalTag);
           if(exists || exists1 ){
             var tags_1 = '"<?php echo $tags; ?>"';
-            //alert(tags_1);
-          var _id = '#'+ pid_1;
-		  
+          var _id = '#'+ pid_1;	  
           $(_id).html("<button type='button' class='reset-button'  id='reset-button-<?php echo $p_id1; ?>' onclick='unshareButton("+pid_1+","+tags_1+");'><i class='fa fa-times' aria-hidden='true'></i> Reset</button>");
 
           }else{
