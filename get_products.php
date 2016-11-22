370a22545cb7497658d1dd6c50d32cabccfa8930
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-22 15:02:57 -0800

    u

--- get_products.php
+++ get_products.php
@@ -318,7 +318,7 @@ catch (shopify\CurlException $e)
 					totolpageno =parseInt(totolpageno.html());
 					console.info(totolpageno);
 				if(parseInt(TotalnoOfProduct) == 0){
-				  $('.content-overflow').html('<span class="cc-text-large no-product-container">No products in '+colhandle+'</span>');
+				  $('.content-overflow').html('<div class="no-product-clearfix"><span class="cc-text-large no-product-container">No products in '+colhandle+'</span></div>');
 			         }
 				   if(totolpageno > 1) {
 					var obj = $('#pagination1').twbsPagination({
