5656fb1a97aba9bd5fedb697bd264923e90b66ff
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-23 19:53:35 -0800

    u

--- get_products.php
+++ get_products.php
@@ -434,8 +434,8 @@ catch (shopify\CurlException $e)
 				// $(this).html("<i class='"+btn_icon[index]+"'  aria-hidden='true'></i> " + btn_text[index]);
 				// });
 				//   $('#share-button-'+pid).text('Share');
-				$('#share-button-'+pid).attr('data-shared','shared');
-				$('.ribbon-'+pid).hide();
+				$('#share-button-<?php echo $p_id1; ?>').attr('data-shared','shared');
+				$('.ribbon-<?php echo $p_id1; ?>').hide();
 
 			}
 		});
@@ -460,12 +460,12 @@ catch (shopify\CurlException $e)
 			success: function(data){
 				var share_id = 'share-'+pid;
 				//        $('.preview-header .btn[data-id='+share_id+']').text('unshared');
-				$('#share-button-'+pid).replaceWith( "<a class='btn grey-button share-button hvr-shutter-out-horizontal' id='reset-button-<?php echo $p_id1; ?>' data-shared='unshared'><i class='fa fa-times' aria-hidden='true'></i> Reset</a>" );
+				$('#share-button-<?php echo $p_id1; ?>').replaceWith( "<a class='btn grey-button share-button hvr-shutter-out-horizontal' id='reset-button-<?php echo $p_id1; ?>' data-shared='unshared'><i class='fa fa-times' aria-hidden='true'></i> Reset</a>" );
 
 				// .html("<i class='fa fa-times' aria-hidden='true'></i> Reset");
 				// $('#share-button-'+pid).addClass("grey-button");
 				// $('#share-button-'+pid).attr('data-shared','unshared');
-				$('.ribbon-'+pid).show();
+				$('.ribbon-<?php echo $p_id1; ?>').show();
 			}
 		});
 	}
