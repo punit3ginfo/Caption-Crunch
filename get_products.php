16a3fe78da64d8737e633970d30e7fa83ab19ed7
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-24 18:10:44 -0800

    u

--- get_products.php
+++ get_products.php
@@ -142,12 +142,11 @@ try
 				</div>
 
 				<?php
-				if ($shared == "unshared") {
-				  echo "<a class='btn green-button share-button hvr-shutter-out-horizontal' id='share-button-$p_id1' data-shared='$shared'><i class='fa fa-bullhorn' aria-hidden='true'></i> Share</a>";
-					  }
-				    elseif{
-			     	($shared == "shared")
+				if ($shared == "shared") {
 				  echo "<a class='btn grey-button share-button hvr-shutter-out-horizontal' id='reset-button-$p_id1' data-shared='$shared'><i class='fa fa-times' aria-hidden='true'></i> Reset</a>";
+					  }
+				    else{
+			          echo "<a class='btn green-button share-button hvr-shutter-out-horizontal' id='share-button-$p_id1' data-shared='$shared'><i class='fa fa-bullhorn' aria-hidden='true'></i> Share</a>"; 
 			  	}
 				?>
 
