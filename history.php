f674d9312302131f069852e5cdf0506fe0a5184e
punit <punit.3ginfo@gmail.com>
2016-11-26 12:07:14 +0530

    Update history.php

--- history.php
+++ history.php
@@ -30,7 +30,7 @@ try
 			$OrigonalTag=$singleproduct['tags'];
 			$ComparePrice=$singleproduct['compare_at_price'];
 			$handle=$singleproduct['handle'];
-             if(strpos($OrigonalTag,'shared') == true)
+             if(strpos($OrigonalTag,'shared') == true || $OrigonalTag=='shared')
 			  {
 				  $shared='unshared';
 			  }
