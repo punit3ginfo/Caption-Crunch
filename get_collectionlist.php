de9d572b349e59e15ad04edc930375b98df35b20
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-26 17:02:57 -0800

    u

--- get_collectionlist.php
+++ get_collectionlist.php
@@ -71,10 +71,10 @@
 		var shop='<?php echo $_REQUEST['shop'] ?>';
 
 		$('product-card-clearfix').addClass('product-card-animate-down');
-		setTimeout(function(){
-			var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
-			$('.content-overflow').html(data);
-		}, 500);
+		// setTimeout(function(){
+		// 	var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
+		// 	$('.content-overflow').html(data);
+		// }, 500);
 
                      var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
 				$('.content-overflow').html(data);
@@ -95,10 +95,10 @@
 		var shop='<?php echo $_REQUEST['shop'] ?>';
 
 		$('product-card-clearfix').addClass('product-card-animate-down');
-		setTimeout(function(){
-			var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
-			$('.content-overflow').html(data);
-		}, 500);
+		// setTimeout(function(){
+		// 	var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
+		// 	$('.content-overflow').html(data);
+		// }, 500);
 
                 $.ajax({
                     url: '/get_sharehistory.php?access_token='+access_token+'&shop='+shop,
@@ -121,10 +121,10 @@
 
 	function search(p_title){
 		$('product-card-clearfix').addClass('product-card-animate-down');
-		setTimeout(function(){
-			var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
-			$('.content-overflow').html(data);
-		}, 500);
+		// setTimeout(function(){
+		// 	var data="<div class='loading-clearfix'><div class='loading-container'><div><img class='loading-img' src='images/loading13.gif' /><span class='cc-text-small loader-text'>Loading...</span></div></div></div>";
+		// 	$('.content-overflow').html(data);
+		// }, 500);
 
                var access_token='<?php echo $access_token ?>';
 		var shop='<?php echo $_REQUEST['shop'] ?>';
