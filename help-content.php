df863d1de294b3a24f5882666c916201da472e81
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-28 19:34:50 -0800

    u

--- help-content.php
+++ help-content.php
@@ -1,109 +1,48 @@
 <form class="form">
 	<!--  Step 1  -->
 	<div class="form-container" style="padding-bottom: 0px">
-
-
 		<div class="form-clearfix" style="border-top: 0px;">
-
 			<span class="cc-text-medium form-text-header account-show-hide-button">
-				Step 1 | Choose Profile
+				Help Topic
 				<i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
-
 		</div>
-
 		<div class="account-show-hide">
 			<div class="form-clearfix">
-
-				<!-- Account Bubbles -->
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-twitter account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-instagram account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-				<!-- Account Bubbles -->
-
+				<span class="cc-text-medium">Help Topics</span><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
 			</div>
 		</div>
 	</div>
 	<!--  Step 2  -->
 	<div class="form-container" style="padding-bottom: 0px">
-		<div id="step2" class="preview-anchor"></div>
-		<div class="form-clearfix"  style="border-top: 0px">
-
-			<span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-				Step 2 | Choose Caption
-				<i id="caption-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+		<div class="form-clearfix" style="border-top: 0px;">
+			<span class="cc-text-medium form-text-header account-show-hide-button">
+				Help Topic
+				<i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
-
 		</div>
-		<div class="caption-show-hide">
+		<div class="account-show-hide">
 			<div class="form-clearfix">
-				<div class="form-caption-list-clearfix">
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-one"><i class="fa fa-check" aria-hidden="true"></i> Caption One</a>
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-two"><i class="fa fa-times" aria-hidden="true"></i> Caption Two</a>
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-three"><i class="fa fa-times" aria-hidden="true"></i> Caption Three</a>
-				</div>
+				<span class="cc-text-medium">Help Topics</span><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
 			</div>
-			<div class="form-clearfix">
-				<span class="cc-text-small">Caption:</span>
-				<div class="caption-textarea-container" style="padding-bottom: 0px;">
-					<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
-				</div>
-				<!-- <span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
-				<div class="caption-textarea-container" style="margin-bottom: 0px;">
-				<textarea id="preview-textarea-small" class="caption-textarea"></textarea>
-			</div> -->
 		</div>
 	</div>
-</div>
-<div class="form-container" style="padding-bottom: 0px">
-	<!--  Step 3  -->
-
-	<div id="step3" class="preview-anchor-2"></div>
-	<div class="form-clearfix"  style="border-top: 0px">
-		<span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-			Step 3 | Add Image
-			<i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-		</span>
+	<!--  3 -->
+	<div class="form-container" style="padding-bottom: 0px">
+		<div class="form-clearfix" style="border-top: 0px;">
+			<span class="cc-text-medium form-text-header account-show-hide-button">
+				Help Topic
+				<i id="account-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+			</span>
+		</div>
+		<div class="account-show-hide">
+			<div class="form-clearfix">
+				<span class="cc-text-medium">Help Topics</span><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+		</div>
 	</div>
-	<div class="image-show-hide">
-
-	</div>
-</div>
 
 </form>
