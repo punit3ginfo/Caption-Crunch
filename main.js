4feddc1b4db3dc2028ae1013fedae96a2796caf6
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-29 15:35:07 -0800

    u

--- main.js
+++ main.js
@@ -24,14 +24,14 @@ $(document).ready(function(){
 
         $('#getting-started-trigger').click(function() {
   	      $('#getting-started-trigger').addClass('menu-container-active');
-  	      $('#share-trigger').removeClass('menu-container-active');
+ //  	      $('#share-trigger').removeClass('menu-container-active');
         });
 
-        $('#share-trigger').click(function() {
-  	      $('#share-trigger').addClass('menu-container-active');
-  	      $('#getting-started-trigger').removeClass('menu-container-active');
-        });
-	
+        // $('#share-trigger').click(function() {
+ //  	      $('#share-trigger').addClass('menu-container-active');
+ //  	      $('#getting-started-trigger').removeClass('menu-container-active');
+        // });
+
 });
 
 // Preview JS
