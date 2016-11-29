df77fbd229c0572ba346df38be11719bb4aabe87
Lucas Sailer <LucasJSailer@gmail.com>
2016-11-29 13:42:57 -0800

    u

--- help.php
+++ help.php
@@ -17,7 +17,7 @@
 					<!-- Collection Card -->
 
 					<a href="javascript:void(0)"  onclick="gettingStartedTirgger();">
-						<div class="menu-containerhelp">
+						<div class="menu-container-help">
 							<div class="menu-clearfix">
 								<div class="menu-font-container">
 									<span class="menu-font">Getting Started</span>
