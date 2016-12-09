49d556a3a24b83ffc59c37846267e74904503d5a
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-09 00:09:39 -0800

    u

--- help-content.php
+++ help-content.php
@@ -4,7 +4,7 @@
 		<div class="form-clearfix" style="border-top: 0px;">
 			<span class="cc-text-medium form-text-header">
 				Getting Started Guide
-				<i id="gs-help-arrow-icon" class="fa fa-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+				<i id="gs-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
 		</div>
 		<div id="gs-help-container" style="border-top: 1px solid #eee; display: none;">
@@ -55,7 +55,7 @@
 		<div class="form-clearfix" style="border-top: 0px;">
 			<span class="cc-text-medium form-text-header">
 				Share
-				<i id="share-help-arrow-icon" class="fa fa-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+				<i id="share-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
 		</div>
 		<div id="share-help-container" style="border-top: 1px solid #eee; display: none;">
@@ -106,7 +106,7 @@
 		<div class="form-clearfix" style="border-top: 0px;">
 			<span class="cc-text-medium form-text-header">
 				Captions Pages
-				<i id="captions-help-arrow-icon" class="fa fa-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+				<i id="captions-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
 		</div>
 		<div id="captions-help-container" style="border-top: 1px solid #eee; display: none;">
