269f5124fbd10d6cbde41504a31142f52fe23cc8
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-15 14:16:37 -0800

    u

--- preview-form.php
+++ preview-form.php
@@ -56,8 +56,13 @@
 
 		<div class="form-clearfix">
 			<span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-				Step 2 | Choose Caption
+				Step 2 | Choose Caption:
 			</span>
+		</div>
+		<div class="form-clearfix">
+			<div class="caption-textarea-container" style="padding-bottom: 0px;">
+				<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
+			</div>
 		</div>
 
 		<!-- <div class="account-show-hide">
