2723798f7a60ffdd943bb583d61bc067b6b0dd14
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-15 15:52:52 -0800

    u

--- preview-form.php
+++ preview-form.php
@@ -5,7 +5,7 @@
 		<div class="form-clearfix" style="border-top: 0px;">
 
 			<span class="cc-text-medium form-text-header account-show-hide-button">
-				Step 1 | Choose Profile:
+				Step 1 | Choose Profile
 			</span>
 
 			<div class="account-bubble-container">
@@ -56,7 +56,7 @@
 
 		<div class="form-clearfix">
 			<span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-				Step 2 | Choose Caption:
+				Step 2 | Choose Caption
 			</span>
 			<div class="caption-textarea-container" style="padding-bottom: 0px;">
 				<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
@@ -71,7 +71,7 @@
 			<div class="add-image-container">
 				<div class="addimage-clearfix">
 					<div id="addimage-image-one" class="addimage-container addimage-active">
-
+						
 					</div>
 				</div>
 
@@ -88,62 +88,4 @@
 			</div>
 		</div>
 	</div>
-	<!--  Step 2  -->
-	<!-- <div class="form-container" style="padding-bottom: 0px">
-		<div id="step2" class="preview-anchor"></div>
-		<div class="form-clearfix"  style="border-top: 0px">
-
-			<span class="cc-text-medium caption-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-				Step 2 | Choose Caption
-				<i id="caption-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-			</span>
-
-		</div>
-		<div class="caption-show-hide">
-			<div class="form-clearfix">
-				<div class="form-caption-list-clearfix">
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-one"><i class="fa fa-check" aria-hidden="true"></i> Caption One</a>
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-two"><i class="fa fa-times" aria-hidden="true"></i> Caption Two</a>
-					<a href="javascript:void(0)" class="btn button-three-col grey-button" id="caption-three"><i class="fa fa-times" aria-hidden="true"></i> Caption Three</a>
-				</div>
-			</div>
-			<div class="form-clearfix">
-				<span class="cc-text-small">Caption:</span>
-				<div class="caption-textarea-container" style="padding-bottom: 0px;">
-					<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
-				</div>
-				<span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
-				<div class="caption-textarea-container" style="margin-bottom: 0px;">
-					<textarea id="preview-textarea-small" class="caption-textarea"></textarea>
-				</div>
-			</div>
-		</div>
-	</div> -->
-
-		<!--  Step 3  -->
-		<!-- <div class="form-container" style="padding-bottom: 0px">
-
-			<div id="step3" class="preview-anchor-2"></div>
-			<div class="form-clearfix"  style="border-top: 0px">
-				<span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-					Step 3 | Add Image
-					<i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-				</span>
-			</div>
-			<div class="image-show-hide">
-				<div class="form-clearfix">
-					<div class="addimage-clearfix">
-						<div id="addimage-image-one" class="addimage-container">
-
-						</div>
-					</div>
-
-					<div class="addimage-clearfix">
-						<div class="addimage-container addimage-active">
-							<i class="fa fa-plus addimage-plus-icon" aria-hidden="true"></i>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div> -->
-	</form>
+</form>
