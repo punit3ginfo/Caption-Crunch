4c86d398bb1285209203070150c765b49c44d047
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-15 14:43:22 -0800

    u

--- preview-form.php
+++ preview-form.php
@@ -82,58 +82,9 @@
 				</div>
 			</div>
 		</div>
-
-		<!-- <div class="account-show-hide">
-			<div class="form-clearfix"> -->
-
-				<!-- Account Bubbles -->
-				<!-- <div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-twitter account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="/#">
-						<div class="account-bubble">
-							<i class="fa fa-instagram account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div> -->
-				<!-- Account Bubbles -->
-<!--
-			</div>
-		</div> -->
-
 	</div>
 	<!--  Step 2  -->
-	<div class="form-container" style="padding-bottom: 0px">
+	<!-- <div class="form-container" style="padding-bottom: 0px">
 		<div id="step2" class="preview-anchor"></div>
 		<div class="form-clearfix"  style="border-top: 0px">
 
@@ -156,37 +107,38 @@
 				<div class="caption-textarea-container" style="padding-bottom: 0px;">
 					<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
 				</div>
-				<!-- <span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
+				<span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
 				<div class="caption-textarea-container" style="margin-bottom: 0px;">
-				<textarea id="preview-textarea-small" class="caption-textarea"></textarea>
-			</div> -->
+					<textarea id="preview-textarea-small" class="caption-textarea"></textarea>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<div class="form-container" style="padding-bottom: 0px">
-	<!--  Step 3  -->
+	</div> -->
 
-	<div id="step3" class="preview-anchor-2"></div>
-	<div class="form-clearfix"  style="border-top: 0px">
-		<span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
-			Step 3 | Add Image
-			<i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
-		</span>
-	</div>
-	<div class="image-show-hide">
-		<div class="form-clearfix">
-			<div class="addimage-clearfix">
-				<div id="addimage-image-one" class="addimage-container">
+		<!--  Step 3  -->
+		<!-- <div class="form-container" style="padding-bottom: 0px">
 
+			<div id="step3" class="preview-anchor-2"></div>
+			<div class="form-clearfix"  style="border-top: 0px">
+				<span class="cc-text-medium image-show-hide-button" style="line-height: 30px; cursor: pointer; width: 100%;">
+					Step 3 | Add Image
+					<i id="image-arrow-icon" class="fa fa-chevron-up" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+				</span>
+			</div>
+			<div class="image-show-hide">
+				<div class="form-clearfix">
+					<div class="addimage-clearfix">
+						<div id="addimage-image-one" class="addimage-container">
+
+						</div>
+					</div>
+
+					<div class="addimage-clearfix">
+						<div class="addimage-container addimage-active">
+							<i class="fa fa-plus addimage-plus-icon" aria-hidden="true"></i>
+						</div>
+					</div>
 				</div>
 			</div>
-
-			<div class="addimage-clearfix">
-				<div class="addimage-container addimage-active">
-					<i class="fa fa-plus addimage-plus-icon" aria-hidden="true"></i>
-				</div>
-			</div>
-</div>
-</div>
-</div>
-</form>
+		</div> -->
+	</form>
