d944d6422eeb8abc5198d595078f6d59e06a0980
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-15 22:41:32 -0800

    u

--- main.js
+++ main.js
@@ -18,10 +18,6 @@ $(document).ready(function(){
 		$('.sidebar-link-active').addClass('hover-active-menu');
 	});
 
-	$('.sidebar-link span').mouseout( function menuHover() {
-		$('.sidebar-link-active').removeClass('hover-active-menu');
-	});
-
 	// $('#dashboard-link').mouseenter( function menuHover() {
 	// 	$('.sidebar-link-active').addClass('hover-active-menu');
 	// });
