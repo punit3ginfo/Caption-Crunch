b61273217e98ce479e80488eee024ad828ede8a3
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-15 16:31:32 -0800

    u

--- preview-form.php
+++ preview-form.php
@@ -3,74 +3,77 @@
 	<div class="form-container">
 
 		<div class="form-clearfix" style="border-top: 0px;">
+			<div class="form-title-container">
+				<span class="cc-text-medium form-text-header account-show-hide-button">
+					Step 1 | Choose Profile
+				</span>
 
-			<span class="cc-text-medium form-text-header account-show-hide-button">
-				Step 1 | Choose Profile
-			</span>
+				<div class="account-bubble-container">
+					<!-- Account Bubbles -->
+					<div class="account-bubble-clearfix">
+						<a href="javascript:void(0)">
+							<div class="account-bubble">
+								<i class="fa fa-facebook account-icon" aria-hidden="true"></i>
+							</div>
+						</a>
+					</div>
 
-			<div class="account-bubble-container">
-				<!-- Account Bubbles -->
-				<div class="account-bubble-clearfix">
-					<a href="javascript:void(0)">
-						<div class="account-bubble">
-							<i class="fa fa-facebook account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
+					<div class="account-bubble-clearfix">
+						<a href="javascript:void(0)">
+							<div class="account-bubble">
+								<i class="fa fa-twitter account-icon" aria-hidden="true"></i>
+							</div>
+						</a>
+					</div>
+
+					<div class="account-bubble-clearfix">
+						<a href="javascript:void(0)">
+							<div class="account-bubble">
+								<i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
+							</div>
+						</a>
+					</div>
+
+					<div class="account-bubble-clearfix">
+						<a href="javascript:void(0)">
+							<div class="account-bubble">
+								<i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
+							</div>
+						</a>
+					</div>
+
+					<div class="account-bubble-clearfix">
+						<a href="javascript:void(0)">
+							<div class="account-bubble">
+								<i class="fa fa-instagram account-icon" aria-hidden="true"></i>
+							</div>
+						</a>
+					</div>
+					<!-- Account Bubbles -->
 				</div>
 
-				<div class="account-bubble-clearfix">
-					<a href="javascript:void(0)">
-						<div class="account-bubble">
-							<i class="fa fa-twitter account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="javascript:void(0)">
-						<div class="account-bubble">
-							<i class="fa fa-google-plus account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="javascript:void(0)">
-						<div class="account-bubble">
-							<i class="fa fa-pinterest-p account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-
-				<div class="account-bubble-clearfix">
-					<a href="javascript:void(0)">
-						<div class="account-bubble">
-							<i class="fa fa-instagram account-icon" aria-hidden="true"></i>
-						</div>
-					</a>
-				</div>
-				<!-- Account Bubbles -->
 			</div>
 
 		</div>
 
 		<div class="form-clearfix">
-			<span class="cc-text-medium caption-show-hide-button" style="line-height: 46px; cursor: pointer; width: 100%;">
-				Step 2 | Choose Caption
-			</span>
-			<div class="account-bubble-container">
-				<select class="form-select">
-					<option>is equal to</option>
-					<option>is not equal to</option>
-					<option>is greater than</option>
-					<option>is less than</option>
-					<option>starts with</option>
-					<option>ends with</option>
-					<option>contains</option>
-					<option>does not contain</option>
-				</select>
+			<div class="form-title-container">
+				<span class="cc-text-medium caption-show-hide-button" style="line-height: 46px; cursor: pointer; width: 100%;">
+					Step 2 | Choose Caption
+				</span>
+				<div class="account-bubble-container">
+					<select class="form-select">
+						<option>is equal to</option>
+						<option>is not equal to</option>
+						<option>is greater than</option>
+						<option>is less than</option>
+						<option>starts with</option>
+						<option>ends with</option>
+						<option>contains</option>
+						<option>does not contain</option>
+					</select>
+				</div>
 			</div>
-
 			<div class="caption-textarea-container">
 				<textarea id="preview-textarea-large" class="caption-textarea"></textarea>
 			</div>
@@ -80,9 +83,11 @@
 		</div>
 
 		<div class="form-clearfix">
-			<span class="cc-text-medium image-show-hide-button" style="line-height: 46px; cursor: pointer; width: 100%;">
-				Step 3 | Add Image
-			</span>
+			<div class="form-title-container">
+				<span class="cc-text-medium image-show-hide-button" style="line-height: 46px; cursor: pointer; width: 100%;">
+					Step 3 | Add Image
+				</span>
+			</div>
 
 			<div class="add-image-container">
 				<div class="addimage-clearfix">
