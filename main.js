b327d1f53491b05e2894f4db425ce639b328281d
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-20 15:18:58 -0800

    u

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 	});
 
 	//  Menu
-	//
+
 	$('.menu-link').mouseenter( function() {
 		$('.menu-container-active').addClass('hover-active-menu');
 	});
@@ -30,9 +30,9 @@ $(document).ready(function(){
 	// 	$('.menu-container-active').removeClass('hover-active-menu');
 	// });
 
-	// $('.menu-container-active').mouseenter( function() {
-	// 	$('.menu-container-active').removeClass('hover-active-menu');
-	// });
+	$('.menu-container-active').mouseenter( function() {
+		$('.menu-container-active').removeClass('hover-active-menu');
+	});
 
 
 	// // Step 1 | Choose Account - Show / Hide
