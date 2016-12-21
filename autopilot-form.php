e3ee164dfedb4ca3d0c182a5b9a228a2dc2f43f4
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-20 17:31:01 -0800

    u

--- autopilot-form.php
+++ autopilot-form.php
@@ -54,62 +54,6 @@
 		</div>
 	</div>
 
-	<div class="form-container" style="height: auto;">
-		<div class="form-clearfix" style="border-top: 0px;">
-			<span class="cc-text-medium form-text-header templates-show-hide-button" style="float: left;">
-				<span class="cc-text-medium form-text-header">
-					Step 2 | Create Template
-				</span>
-			</span>
-		</div>
-	<div class="template-show-hide">
-		<div class="form-clearfix">
-			<div class="form-caption-list-clearfix">
-				<a id="product-title-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-title-icon" class="fa fa-times" aria-hidden="true"></i> Product Title
-				</a>
-				<a id="product-price-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-price-icon" class="fa fa-times" aria-hidden="true"></i> Product Price
-				</a>
-				<a id="product-comp-price-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-comp-price-icon" class="fa fa-times" aria-hidden="true"></i> Compare Price
-				</a>
-				<a id="product-type-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i  id="product-type-icon" class="fa fa-times" aria-hidden="true"></i> Product Type
-				</a>
-				<a id="product-url-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-url-icon" class="fa fa-times" aria-hidden="true"></i> Product Url
-				</a>
-				<a id="product-description-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-description-icon" class="fa fa-times" aria-hidden="true"></i> Description
-				</a>
-				<a id="product-collection-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-collection-icon" class="fa fa-times" aria-hidden="true"></i> Collection Title
-				</a>
-				<a id="product-percent-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-percent-icon" class="fa fa-times" aria-hidden="true"></i> Precent Off
-				</a>
-				<a id="product-dollars-btn" href="javascript:void(0)" class="btn button-three-col grey-button">
-					<i id="product-dollars-icon" class="fa fa-times" aria-hidden="true"></i> Dollars Saved
-				</a>
-			</div>
-		</div>
-		<div class="form-clearfix" style="padding-bottom: 0px;">
-			<span class="cc-text-small">Caption for Facebook, Google+, & Pinterest</span>
-			<div class="caption-textarea-container" style="padding-bottom: 0px;">
-				<textarea id="caption-textarea-large" class="caption-textarea"></textarea>
-			</div>
-			<span class="cc-text-small">Caption for Twitter</span>
-			<div class="twitter-textarea-container">
-				<textarea id="caption-textarea-small" class="twitter-caption-textarea"></textarea>
-			</div>
-			<!-- <span class="cc-text-small">Caption for: Twitter (Character Limit: 140)</span>
-			<div class="caption-textarea-container" style="margin-bottom: 0px;">
-			<textarea id="caption-textarea-small" class="caption-textarea"></textarea>
-		</div> -->
-	</div>
-</div>
-</div>
 <!-- CONDITIONS -->
 <div class="form-container">
 	<div class="form-clearfix" style="border-top: 0px;">
