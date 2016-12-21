877860b156f70cc5f8ecdfebba354d587eb2fa8f
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-21 13:09:29 -0800

    u

--- collections.php
+++ collections.php
@@ -101,7 +101,7 @@
 				</div> -->
 		</div>
 		<div class="content-overflow-container">
-			<div id="product-feed" class="content-overflow">
+			<div data-aos="fade-up" id="product-feed" class="content-overflow">
 
 				<?php
 
