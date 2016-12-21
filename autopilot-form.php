ccaf4236dbda5c23dcd5e92a38f6899cc577f764
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-21 15:36:14 -0800

    u

--- autopilot-form.php
+++ autopilot-form.php
@@ -1,10 +1,60 @@
 <form class="form">
-	<!-- TEMPLATE -->
+	<!-- SETTINGS -->
+	<div class="form-container">
+		<div class="form-clearfix" style="border-top: 0px;">
+			<span class="cc-text-medium form-text-header conditions-show-hide-button">
+				Step 1 | Campaign Settings
+			</span>
+		</div>
+		<div class="conditions-show-hide">
+			<div class="form-clearfix flex-col">
+				<span class="cc-text-small" style="line-height: 46px;">Post Frequency :</span>
+				<div class="priority-container">
+					<select class="form-select priority-dropdown">
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+					</select>
+					<select class="form-select frequecy-dropdown">
+						<option>Daily</option>
+						<option>Bi-weekly</option>
+						<option>Weekly</option>
+						<option>Monthly</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-clearfix flex-col">
+				<span class="cc-text-small" style="line-height: 46px;">Push new products to front of queue</span>
+
+				<a href="javascript:void(0)" class="btn grey-button save-button">
+					<i class="fa fa-times" aria-hidden="true"></i>
+					NO
+				</a>
+			</div>
+			<div class="form-clearfix flex-col">
+				<span class="cc-text-small" style="line-height: 46px;">Repeat campaign after all products have been shared once</span>
+
+				<a href="javascript:void(0)" class="btn green-button save-button">
+					<i class="fa fa-check" aria-hidden="true"></i>
+					YES
+				</a>
+			</div>
+		</div>
+
+	</div>
+
 	<div class="form-container" style="height: auto;">
 		<div class="form-clearfix" style="border-top: 0px; padding-bottom: 0px;">
 			<div class="form-title-container">
 				<span class="cc-text-medium form-text-header account-show-hide-button">
-					Step 1 | Select Profiles
+					Step 2 | Select Profiles
 				</span>
 
 				<div class="account-bubble-container">
@@ -53,56 +103,7 @@
 			</div>
 		</div>
 	</div>
-	<!-- SETTINGS -->
-	<div class="form-container">
-		<div class="form-clearfix" style="border-top: 0px;">
-			<span class="cc-text-medium form-text-header conditions-show-hide-button">
-				Step 2 | Campaign Settings
-			</span>
-		</div>
-		<div class="conditions-show-hide">
-			<div class="form-clearfix flex-col">
-				<span class="cc-text-small" style="line-height: 46px;">Post Frequency :</span>
-				<div class="priority-container">
-					<select class="form-select priority-dropdown">
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-						<option>5</option>
-						<option>6</option>
-						<option>7</option>
-						<option>8</option>
-						<option>9</option>
-						<option>10</option>
-					</select>
-					<select class="form-select frequecy-dropdown">
-						<option>Daily</option>
-						<option>Bi-weekly</option>
-						<option>Weekly</option>
-						<option>Monthly</option>
-					</select>
-				</div>
-			</div>
-			<div class="form-clearfix flex-col">
-				<span class="cc-text-small" style="line-height: 46px;">Push new products to front of queue</span>
 
-				<a href="javascript:void(0)" class="btn grey-button save-button">
-					<i class="fa fa-times" aria-hidden="true"></i>
-					NO
-				</a>
-			</div>
-			<div class="form-clearfix flex-col">
-				<span class="cc-text-small" style="line-height: 46px;">Repeat campaign after all products have been shared once</span>
-
-				<a href="javascript:void(0)" class="btn green-button save-button">
-					<i class="fa fa-check" aria-hidden="true"></i>
-					YES
-				</a>
-			</div>
-		</div>
-
-	</div>
 <!-- CONDITIONS -->
 <div class="form-container">
 	<div class="form-clearfix" style="border-top: 0px;">
