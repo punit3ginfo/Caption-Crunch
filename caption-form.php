a21579f3e87b84b0946a91a44a0d6d7c9199565b
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-20 16:38:15 -0800

    u

--- caption-form.php
+++ caption-form.php
@@ -118,6 +118,24 @@
 			Step 3 | Set  Product Conditions
 		</span>
 
+		<div class="priority-container">
+			<span class="cc-text-small">
+				Priority :
+			</span>
+			<select class="form-select">
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+			</select>
+		</div>
+
 	</div>
 	<div class="conditions-show-hide">
 		<div class="form-clearfix">
