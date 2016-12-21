866723977027943081e1249f3ba17280123987ba
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-21 13:39:58 -0800

    u

--- autopilot-form.php
+++ autopilot-form.php
@@ -52,9 +52,31 @@
 				</div>
 			</div>
 		</div>
-		<div class="form-clearfix flex-col" style="border-top: 0px; padding-bottom: 0px;">
+		<div class="form-clearfix flex-col" style="padding-bottom: 0px;">
 			<span class="cc-text-small">Post Frequency :</span>
-
+			<div class="priority-container">
+				<span class="cc-text-small" style=" width: 70px; line-height: 46px;">
+					Priority :
+				</span>
+				<select class="form-select priority-dropdown">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+				</select>
+				<select class="form-select priority-dropdown">
+					<option>Daily</option>
+					<option>Bi-weekly</option>
+					<option>Weekly</option>
+					<option>Monthly</option>
+				</select>
+			</div>
 		</div>
 	</div>
 	<!-- SETTINGS -->
