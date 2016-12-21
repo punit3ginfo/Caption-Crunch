6a6673e428e59db53eeec3dc79d3cfa25e7d9032
Lucas Sailer <LucasJSailer@gmail.com>
2016-12-21 15:40:21 -0800

    u

--- captions.php
+++ captions.php
@@ -41,10 +41,6 @@
 		</div>
 
 		<div class="content-header-3col-container">
-			<div class="content-header-container">
-				<input class="search-text-box" onKeyDown="if(event.keyCode==13) search(document.getElementById('formValueId').value);" id="formValueId" type="text" name="q_name" placeholder="Search" />
-				<i class="fa fa-search search-button" onclick="search(document.getElementById('formValueId').value);"></i>
-			</div>
 		</div>
 
 		<div class="content-header-3col-container">
