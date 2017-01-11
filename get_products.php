d325f3446c5efd5f3584eb99ca63083912fae265
punit <punit.3ginfo@gmail.com>
2017-01-11 13:57:12 +0530

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -3,7 +3,6 @@
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token=$_REQUEST['access_token'];
-echo "<script>alert('1');alert(".$access_token.");</script>";
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 
 ?>
