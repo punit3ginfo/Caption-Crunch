dd401eeea107ca655dea0ea354a3126fce4fc179
punit <punit.3ginfo@gmail.com>
2017-01-12 16:23:39 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,17 @@
 <!-- <iframe style="width: 100%; height: 100%;" src="https://buffer.com"></iframe> -->
-
+<?php
+require __DIR__.'/buffer-master/buffer-connect.php';
+?>
 <div class="dashboard">
-
+<?php if (!$buffer->ok) {
+		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
+	} else {
+		$profiles = $buffer->go('/profiles');
+			
+		if (is_array($profiles)) {
+			foreach ($profiles as $profile) {
+				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
+			}
+		}
+	} ?>
 </div>
