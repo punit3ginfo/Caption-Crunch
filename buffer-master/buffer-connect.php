3f6c6ba9fffd134b6ed89346d42ee93890ee29e3
punit <punit.3ginfo@gmail.com>
2017-01-12 16:23:22 +0530

    Update buffer-connect.php

--- buffer-master/buffer-connect.php
+++ buffer-master/buffer-connect.php
@@ -7,16 +7,4 @@
 	$callback_url = 'https://share-tag.herokuapp.com';
 	
 	$buffer = new BufferApp($client_id, $client_secret, $callback_url);
-		
-	if (!$buffer->ok) {
-		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
-	} else {
-		$profiles = $buffer->go('/profiles');
-			
-		if (is_array($profiles)) {
-			foreach ($profiles as $profile) {
-				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
-			}
-		}
-	}
 ?>
