c1e3e99f02bd6b669a8d4d6129c3348aceebaa91
Lucas Sailer <LucasJSailer@gmail.com>
2017-01-11 18:35:32 -0800

    u

--- help-content.php
+++ help-content.php
@@ -54,7 +54,7 @@
 	<div class="form-container">
 		<div class="form-clearfix" style="border-top: 0px;">
 			<span class="cc-text-medium form-text-header">
-				Share
+				Buffer
 				<i id="share-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
 		</div>
@@ -105,7 +105,7 @@
 	<div class="form-container">
 		<div class="form-clearfix" style="border-top: 0px;">
 			<span class="cc-text-medium form-text-header">
-				Captions Pages
+				Autopilot
 				<i id="captions-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
 			</span>
 		</div>
@@ -152,5 +152,109 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="form-container">
+		<div class="form-clearfix" style="border-top: 0px;">
+			<span class="cc-text-medium form-text-header">
+				Captions
+				<i id="captions-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+			</span>
+		</div>
+		<div id="captions-help-container" style="border-top: 1px solid #eee;">
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="form-container">
+		<div class="form-clearfix" style="border-top: 0px;">
+			<span class="cc-text-medium form-text-header">
+				Products
+				<i id="captions-help-arrow-icon" class="icon-chevron-down icon-rotate" aria-hidden="true" style="float: right; line-height: 46px;"></i>
+			</span>
+		</div>
+		<div id="captions-help-container" style="border-top: 1px solid #eee;">
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+			<div class="form-clearfix form-help">
+				<div class="anchor"></div>
+				<span class="cc-text-medium">Help Topics</span><br /><br />
+				<span class="cc-text-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+			</div>
+		</div>
+	</div>
+
+
 
 </form>
