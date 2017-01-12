e81a184843f2b315bb8d2c85e37d9dd8ff2991d6
punit <punit.3ginfo@gmail.com>
2017-01-12 15:34:14 +0530

    Update buffer-connect.php

--- buffer-master/buffer-connect.php
+++ buffer-master/buffer-connect.php
@@ -2,9 +2,9 @@
 	session_start();
 	require('buffer.php');
 	
-	$client_id = '';
-	$client_secret = '';
-	$callback_url = 'http://127.0.0.1/callback';
+	$client_id = '587754240ad90d54502a8e09';
+	$client_secret = 'ab798c2379a4a55a04d27c09ceae6f58';
+	$callback_url = 'https://share-tag.herokuapp.com';
 	
 	$buffer = new BufferApp($client_id, $client_secret, $callback_url);
 		
