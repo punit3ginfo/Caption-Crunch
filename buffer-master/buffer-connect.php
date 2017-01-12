b5e79ccb3c68152fba0995fc0c1f4df9e302f861
punit <punit.3ginfo@gmail.com>
2017-01-12 15:29:35 +0530

    Create buffer-connect.php

---
+++ buffer-master/buffer-connect.php
@@ -0,0 +1,22 @@
+<?
+	session_start();
+	require('buffer.php');
+	
+	$client_id = '';
+	$client_secret = '';
+	$callback_url = 'http://127.0.0.1/callback';
+	
+	$buffer = new BufferApp($client_id, $client_secret, $callback_url);
+		
+	if (!$buffer->ok) {
+		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
+	} else {
+		$profiles = $buffer->go('/profiles');
+			
+		if (is_array($profiles)) {
+			foreach ($profiles as $profile) {
+				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
+			}
+		}
+	}
+?>
